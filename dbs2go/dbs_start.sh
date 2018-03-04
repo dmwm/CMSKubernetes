a74a1ec604e46ff9c79b9ff75e41ec7c5fafe538
Valentin Kuznetsov <vkuznet@gmail.com>
2018-03-04 10:15:58 -0500

    update

--- dbs2go/dbs_start.sh
+++ dbs2go/dbs_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # start dbs2go server
 cd $GOPATH/src/github.com/vkuznet/dbs2go
-dbs2go -dbfile /tmp/dbs2go/dbfile.reader 2>&1 1>& dbs.log
+dbs2go -dbfile /etc/secrets/dbfile 2>&1 1>& dbs.log
