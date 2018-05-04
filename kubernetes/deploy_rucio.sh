64070b91b3e6b093b3388b9bbd20b9ffdb7bf122
Eric Vaandering <ericvaandering@gmail.com>
2018-05-04 16:53:19 -0500

    Deploy script and try to open a 2nd port 80

---
+++ kubernetes/deploy_rucio.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+kubectl create secret generic dburl --from-file=DBURL.txt
+
+kubectl replace --force -f rucio_server_cms.yaml
+
+
+kubectl -n kube-system delete configmap traefik-conf
+kubectl -n kube-system create configmap traefik-conf --from-file=rucio-traefik.toml
+
+kubectl delete daemonset ingress-traefik -n kube-system
+kubectl delete service ingress-traefik -n kube-system
+kubectl apply -f rucio-traefik.yaml
+
+
