a8939b6fab59f9171a24b24c7e51affbb472ae1c
Valentin Kuznetsov <vkuznet@gmail.com>
2018-05-08 12:31:16 -0400

    fix start-up script

--- docker/reqmgr2/run.sh
+++ docker/reqmgr2/run.sh
@@ -3,14 +3,12 @@ if [ -f /etc/grid-security/hostkey.pem ]; then
     # overwrite host PEM files in /data/certs since we used them during installation time
     sudo cp /etc/grid-security/hostkey.pem /data/certs/
     sudo cp /etc/grid-security/hostcert.pem /data/certs/
-    /data/cfg/admin/InstallDev -s start
-    ps auxw | grep httpd
-    while true
-    do
-        log=`ls -t /data/srv/logs/reqmgr2/access_log* | head -1`
-        tail -1 $log
-        sleep 1
-    done
-else
-    echo "Unable to start frontend, host PEM files are not found"
 fi
+/data/srv/current/config/reqmgr2/manage start 'I did read documentation'
+ps auxw | grep httpd
+while true
+do
+    log=`ls -t /data/srv/logs/reqmgr2/reqmgr2*.log | head -1`
+    tail -1 $log
+    sleep 1
+done
