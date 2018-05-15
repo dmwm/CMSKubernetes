d34a2bedc556636b77b489fc938b9477551c5c3b
Valentin <vkuznet@gmail.com>
2018-05-15 15:32:52 +0200

    Use explicit port until TLS will be fixed

--- docker/exporters/run.sh
+++ docker/exporters/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # start exporter servers
-nohup $WDIR/bin/das2go_exporter -uri https://cmsweb-k8s.web.cern.ch/das/status 2>&1 1>& das2go_exporter.log < /dev/null &
+nohup $WDIR/bin/das2go_exporter -uri https://cmsweb-k8s.web.cern.ch:31443/das/status 2>&1 1>& das2go_exporter.log < /dev/null &
 # start node exporter
 nohup $WDIR/bin/node_exporter 2>&1 1>& node_exporter.log < /dev/null &
 #tail -f *exporter.log
