67f2e908b0f32344eddc33511e6c494eea2107df
Valentin <vkuznet@gmail.com>
2018-05-17 18:20:32 +0200

    Remove proxy since we can use internal HTTP calls within k8s

--- docker/exporters/run.sh
+++ docker/exporters/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# get proxy
+echo "USER=$USER"
+sudo /data/proxy.sh $USER
+ls -la /data/srv/current/auth/proxy/proxy
 # start exporter servers
-nohup $WDIR/bin/das2go_exporter -uri https://cmsweb-k8s.web.cern.ch:31443/das/status 2>&1 1>& das2go_exporter.log < /dev/null &
+nohup $WDIR/bin/das2go_exporter -uri http://cmsweb-k8s.web.cern.ch:8212/status 2>&1 1>& das2go_exporter.log < /dev/null &
 # start node exporter
 nohup $WDIR/bin/node_exporter 2>&1 1>& node_exporter.log < /dev/null &
-#tail -f *exporter.log
 # start grafana server
 cd $WDIR/grafana
 #nohup ./bin/grafana-server web 2>&1 1>& grafana.log < /dev/null &
