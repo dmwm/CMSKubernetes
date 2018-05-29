30202b75e7f1b178cdf5fbe139f91c802ef9a1c6
Valentin <vkuznet@gmail.com>
2018-05-29 16:40:05 +0200

    Add proxy/probe and refactor install procedure

--- docker/exporters/run.sh
+++ docker/exporters/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 # get proxy
-echo "USER=$USER"
-sudo /data/proxy.sh $USER
-ls -la /data/srv/current/auth/proxy/proxy
+/data/proxy.sh $USER
+sleep 2
+
 # start exporter servers
 nohup $WDIR/bin/das2go_exporter -uri http://cmsweb-k8s.web.cern.ch:8212/status 2>&1 1>& das2go_exporter.log < /dev/null &
+nohup $WDIR/bin/reqmgr_exporter -namespace reqmgr -uri https://cmsweb-k8s.web.cern.ch:31443/reqmgr2/data/proc_status 2>&1 1>& reqmgr_exporter.log < /dev/null &
 # start node exporter
 nohup $WDIR/bin/node_exporter 2>&1 1>& node_exporter.log < /dev/null &
 # start grafana server
