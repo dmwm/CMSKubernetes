6389e74f9a8e7c41e48890d2b47f720bb745ffef
Valentin <vkuznet@gmail.com>
2018-09-12 19:39:16 +0200

    Add changes to deploy stand-along tfaas service

--- docker/tfaas/run.sh
+++ docker/tfaas/run.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# get proxy
-echo "USER=$USER"
-/data/proxy.sh $USER
-ls -la /data/srv/current/auth/proxy/proxy
-sleep 2
 # start server
-if [ -f /etc/secrets/tfaas-config.json ]; then
-    echo "Start with /etc/secrets/tfaas-config.json"
-    cat /etc/secrets/tfaas-config.json
-    tfaas -config /etc/secrets/tfaas-config.json
-else
-    echo "Start with $PWD/config.json"
-    cat $PWD/config.json
-    tfaas -config $PWD/config.json
-fi
+echo "Start with $PWD/config.json"
+cat $PWD/config.json
+tfaas -config $PWD/config.json
