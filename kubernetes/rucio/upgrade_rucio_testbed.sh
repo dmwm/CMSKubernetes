5b20d5669c8132381c4ad25f54f4d636ebff6034
Eric Vaandering <ericvaandering@gmail.com>
2018-12-19 22:11:45 +0100

    Update some settings for CMS specific stuff

--- kubernetes/rucio/upgrade_rucio_testbed.sh
+++ kubernetes/rucio/upgrade_rucio_testbed.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-helm upgrade --values rucio-graphite.yaml,rucio-graphite-ingress.yaml,rucio-graphite-pvc.yaml  graphite stable/graphite 
+helm upgrade --values rucio-graphite.yaml  graphite stable/graphite # Don't do PVC again
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-server-testbed.yaml,cms-rucio-testbed-db.yaml cms-rucio-testbed rucio/rucio-server
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-testbed-db.yaml cms-ruciod-testbed rucio/rucio-daemons
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-testbed-db.yaml cms-analysisd-testbed rucio/rucio-daemons
