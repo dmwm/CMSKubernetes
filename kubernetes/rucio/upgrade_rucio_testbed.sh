cf616a665c54f2ba2c71ad8db2fac3b5c430d305
Eric Vaandering <ericvaandering@gmail.com>
2019-02-01 22:29:57 +0100

    Switch filebeat and logstash to helm

--- kubernetes/rucio/upgrade_rucio_testbed.sh
+++ kubernetes/rucio/upgrade_rucio_testbed.sh
@@ -5,4 +5,9 @@ helm upgrade --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-serv
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-testbed-db.yaml cms-ruciod-testbed rucio/rucio-daemons
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-testbed-db.yaml cms-analysisd-testbed rucio/rucio-daemons
 
+# Filebeat and logstash
+
+helm upgrade --values cms-rucio-logstash.yml,logstash-filter-testbed.yml logstash stable/logstash
+helm upgrade --values cms-rucio-filebeat.yml filebeat stable/filebeat
+
 kubectl get pods
