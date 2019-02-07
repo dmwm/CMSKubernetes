b0b89e4f4b72ff0c97fd39e1696e530a5532aac2
Eric Vaandering <ericvaandering@gmail.com>
2019-02-07 08:02:30 -0600

    Update scripts for extra yaml files

--- kubernetes/rucio/upgrade_rucio_bleeding.sh
+++ kubernetes/rucio/upgrade_rucio_bleeding.sh
@@ -5,7 +5,7 @@
 #helm upgrade --values rucio-graphite.yaml  graphite kiwigrid/graphite # Don't do PVC again
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-server-oldtest.yaml,cms-rucio-oldtest-db.yaml cms-rucio-testbed  ~/rucio-helm-charts/rucio-server
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml cms-ruciod-testbed ~/rucio-helm-charts/rucio-daemons
-helm upgrade --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-oldtest-db.yaml cms-analysisd-testbed  ~/rucio-helm-charts/rucio-daemons
+helm upgrade --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml cms-analysisd-testbed ~/rucio-helm-charts/rucio-daemons
 
 # Filebeat and logstash
 
