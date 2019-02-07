7a04c2d11a24929882a665ead4b8c59831a88a0c
Eric Vaandering <ericvaandering@gmail.com>
2019-02-07 14:11:50 +0100

    Add a development server

---
+++ kubernetes/rucio/upgrade_rucio_dev.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#helm upgrade --values rucio-graphite.yaml  graphite kiwigrid/graphite # Don't do PVC again
+helm upgrade --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-server-dev.yaml,cms-rucio-dev-db.yaml cms-rucio-testbed rucio/rucio-server
+helm upgrade --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-daemons-dev.yaml,cms-rucio-dev-db.yaml cms-ruciod-testbed rucio/rucio-daemons
+helm upgrade --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-daemons-dev.yaml,cms-rucio-dev-db.yaml cms-analysisd-testbed rucio/rucio-daemons
+
+# Filebeat and logstash
+
+helm upgrade --values cms-rucio-logstash.yml,logstash-filter-dev.yml logstash stable/logstash
+helm upgrade --values cms-rucio-filebeat.yml filebeat stable/filebeat
+
+kubectl get pods
