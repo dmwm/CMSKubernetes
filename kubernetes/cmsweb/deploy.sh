e09399cf691a6da17065aa0eed0122f2d74225c4
Valentin <vkuznet@gmail.com>
2019-03-14 21:23:06 +0100

    Re-design deploy script

--- kubernetes/cmsweb/deploy.sh
+++ kubernetes/cmsweb/deploy.sh
@@ -1,145 +1,227 @@
 #!/bin/bash
+##H Usage: deploy.sh ACTION
+##H
+##H Available actions:
+##H   help       show this help
+##H   cleanup    cleanup services
+##H   create     create services
+##H
 
 cluster=cmsweb
 host=`openstack --os-project-name "CMS Webtools Mig" coe cluster show $cluster | grep node_addresses | awk '{print $4}' | sed -e "s,\[u',,g" -e "s,'\],,g"`
 kubehost=`host $host | awk '{print $5}' | sed -e "s,ch.,ch,g"`
 echo "Kubernetes host: $kubehost"
-echo "generate hmac secret"
-hmac=$PWD/hmac.random
-perl -e 'open(R, "< /dev/urandom") or die; sysread(R, $K, 20) or die; print $K' > $hmac
 
-pkgs="das dbs ing frontend couchdb reqmgr httpsgo reqmon workqueue tfaas exporters"
-pkgs="dbs ing frontend"
+cleanup()
+{
+    echo "### DELETE ACTION ###"
+    echo "--- delete secrets"
+    # new method to delete secrets
+    secrets=`kubectl get secrets | grep -v NAME | grep -v default | awk '{print $1}'`
+    for s in $secrets; do
+        kubectl delete secret/$s
+    done
 
-echo "### secrets"
-for p in $pkgs; do
-    kubectl delete secret/${p}-secrets
-done
-kubectl delete secret/dbs2go-secrets
-kubectl delete secret/cluster-tls-cert
-#kubectl -n kube-system delete secret/cluster-tls-cert
-#kubectl -n kube-system delete secret/traefik-cert
-#kubectl -n kube-system delete configmap traefik-conf
+    echo
+    echo "--- delete services"
+    services=`kubectl get svc | grep -v NAME | grep -v default | awk '{print $1}'`
+    for s in $services; do
+        if [ -f ${s}.yaml ]; then
+            kubectl delete -f ${s}.yaml
+        fi
+    done
 
-sleep 2
-
-# prepare secrets
-dbsconfig=dbsconfig.json
-dasconfig=dasconfig.json
-tfaasconfig=tfaas-config.json
-httpsgoconfig=httpsgoconfig.json
-user_crt=/afs/cern.ch/user/v/valya/.globus/usercert.pem
-robot_key=/afs/cern.ch/user/v/valya/private/certificates/robotkey.pem
-robot_crt=/afs/cern.ch/user/v/valya/private/certificates/robotcert.pem
-server_key=/afs/cern.ch/user/v/valya/private/certificates/server.key
-server_crt=/afs/cern.ch/user/v/valya/private/certificates/server.crt
-cmsweb_key=/afs/cern.ch/user/v/valya/private/certificates/cmsweb-hostkey.pem
-cmsweb_crt=/afs/cern.ch/user/v/valya/private/certificates/cmsweb-hostcert.pem
-dbfile=/afs/cern.ch/user/v/valya/private/dbfile
-dbssecrets=/afs/cern.ch/user/v/valya/private/DBSSecrets.py
-./make_das_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dasconfig
-./make_dbs_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dbssecrets
-./make_dbs2go_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dbsconfig $dbfile
-./make_frontend_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
-./make_couchdb_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
-./make_reqmgr_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
-./make_reqmon_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
-./make_workqueue_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
-./make_tfaas_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $tfaasconfig
-./make_exporters_secret.sh $robot_key $robot_crt
-./make_httpsgo_secret.sh $httpsgoconfig
-
-echo "### apply secrets"
-for p in $pkgs; do
-    kubectl apply -f ${p}-secrets.yaml --validate=false
-done
-kubectl apply -f dbs2go-secrets.yaml --validate=false
-rm *secrets.yaml $hmac
-
-echo "### create secrets for TLS case"
-# generate tls.key/tls.crt for custom CA
-# openssl genrsa -out tls.key 3072 -config openssl.cnf; openssl req -new -x509 -key tls.key -sha256 -out tls.crt -days 730 -config openssl.cnf -subj "/CN=cmsweb-test.web.cern.ch"
-
-# generate tls.key/tls.crt
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=cmsweb-test.web.cern.ch"
-
-# create secret with our tls.key/tls.crt
-#kubectl create secret tls cluster-tls-cert --key=tls.key --cert=tls.crt
-
-# create secret with our key/crt (they can be generated at ca.cern.ch/ca, see Host certificates)
-kubectl create secret tls cluster-tls-cert --key=$cmsweb_key --cert=$cmsweb_crt
-
-sleep 2
-
-kubectl get secrets
-kubectl -n kube-system get secrets
-kubectl -n kube-system get configmap
-
-echo
-echo "### label node"
-for n in `kubectl get nodes | grep -v master | awk '{print $1}'`; do
-    kubectl label node $n role=ingress --overwrite
-    kubectl get node -l role=ingress
-done
-
-#clsname=`kubectl get nodes | tail -1 | awk '{print $1}'`
-#kubectl label node $clsname role=ingress --overwrite
-#kubectl get node -l role=ingress
-
-echo
-echo "### delete services"
-for p in $pkgs; do
-    if [ -f ${p}.yaml ]; then
-        kubectl delete -f ${p}.yaml
+    echo
+    echo "--- delete daemon ingress-traefik"
+    if [ -n "`kubectl get daemonset -n kube-system | grep ingress-traefik`" ]; then
+        if [ -n "`kubectl -n kube-system get svc | grep ingress-traefik`" ]; then
+            kubectl -n kube-system delete svc ingress-traefik
+        fi
+        if [ -n "`kubectl -n kube-system get ds | grep ingress-traefik`" ]; then
+            kubectl -n kube-system delete daemonset ingress-traefik
+        fi
     fi
-done
-kubectl delete -f httpgo.yaml
-kubectl delete -f das2go.yaml
-kubectl delete -f dbs2go.yaml
 
-sleep 2
-
-echo
-echo "### deploy services"
-for p in $pkgs; do
-    if [ -f ${p}.yaml ]; then
-        kubectl apply -f ${p}.yaml --validate=false
+    # check if there are existing prometheus services around, if so we'll delete them
+    echo
+    echo "--- delete monitoring services"
+    if [ -n "`kubectl -n monitoring get svc | grep prometheus-service`" ]; then
+        kubectl -n monitoring delete -f kubernetes-prometheus/prometheus-service.yaml
     fi
-done
-kubectl apply -f httpgo.yaml --validate=false
-kubectl apply -f das2go.yaml --validate=false
-kubectl apply -f dbs2go.yaml --validate=false
+    if [ -n "`kubectl -n monitoring get pods | grep prometheus-deployment`" ]; then
+        kubectl -n monitoring delete -f kubernetes-prometheus/prometheus-deployment.yaml
+    fi
+    if [ -n "`kubectl -n monitoring get configmap | grep config`" ]; then
+        kubectl -n monitoring delete -f kubernetes-prometheus/config-map.yaml
+    fi
 
-sleep 2
-echo
-echo "### delete daemon ingress-traefik"
-if [ -n "`kubectl get daemonset -n kube-system | grep ingress-traefik`" ]; then
-    kubectl -n kube-system delete svc ingress-traefik
-    kubectl -n kube-system delete daemonset ingress-traefik
-fi
-sleep 2
-echo "### deploy traefik"
-#kubectl -n kube-system create secret generic traefik-cert --from-file=$server_crt --from-file=$server_key
-#kubectl -n kube-system create configmap traefik-conf --from-file=traefik.toml
-kubectl -n kube-system apply -f traefik.yaml --validate=false
+}
 
-echo "deploy prometheus: https://devopscube.com/setup-prometheus-monitoring-on-kubernetes/"
-sleep 2
+check()
+{
+    echo
+    for ns in "monitoring" "kube-system"; do
+        echo
+        echo "*** check pods in $ns namespace"
+        kubectl get pods -n $ns
+        echo
+        echo "*** check services in $ns namesapce"
+        kubectl get svc -n $ns
+        echo
+        echo "*** check pods in $ns namespace"
+        kubectl get secrets -n $ns
+    done
 
-kubectl -n monitoring delete -f kubernetes-prometheus/prometheus-service.yaml
-kubectl -n monitoring delete -f kubernetes-prometheus/prometheus-deployment.yaml
-kubectl -n monitoring delete -f kubernetes-prometheus/config-map.yaml
+    echo
+    echo "*** check pods"
+    kubectl get pods
+    echo
+    echo "*** check services"
+    kubectl get svc
+    echo
+    echo "*** check secrets"
+    kubectl get secrets
+    echo "*** check ingress"
+    kubectl get ing
 
-if [ -z "`kubectl get namespaces | grep monitoring`" ]; then
-    kubectl create namespace monitoring
-fi
-kubectl -n monitoring apply -f kubernetes-prometheus/config-map.yaml --validate=false
-kubectl -n monitoring apply -f kubernetes-prometheus/prometheus-deployment.yaml --validate=false
-kubectl -n monitoring apply -f kubernetes-prometheus/prometheus-service.yaml --validate=false
-kubectl -n monitoring get deployments
-kubectl -n monitoring get pods
-prom=`kubectl -n monitoring get pods | grep prom | awk '{print $1}'`
-echo "### we may access prometheus locally as following"
-echo "kubectl -n monitoring port-forward $prom 8080:9090"
-echo "### to access prometheus externally we should do the following:"
-echo "ssh -S none -L 30000:$kubehost:30000 $USER@lxplus.cern.ch"
+}
+
+create()
+{
+    # adjust as necessary
+    pkgs="das dbs ing frontend couchdb reqmgr httpsgo reqmon workqueue tfaas exporters"
+    pkgs="ing frontend dbs das2go"
+
+    # adjust as necessary
+    user_crt=/afs/cern.ch/user/v/valya/.globus/usercert.pem
+    robot_key=/afs/cern.ch/user/v/valya/private/certificates/robotkey.pem
+    robot_crt=/afs/cern.ch/user/v/valya/private/certificates/robotcert.pem
+    server_key=/afs/cern.ch/user/v/valya/private/certificates/server.key
+    server_crt=/afs/cern.ch/user/v/valya/private/certificates/server.crt
+    cmsweb_key=/afs/cern.ch/user/v/valya/private/certificates/cmsweb-hostkey.pem
+    cmsweb_crt=/afs/cern.ch/user/v/valya/private/certificates/cmsweb-hostcert.pem
+    dbfile=/afs/cern.ch/user/v/valya/private/dbfile
+    dbssecrets=/afs/cern.ch/user/v/valya/private/DBSSecrets.py
+
+    echo "### CREATE ACTION ###"
+    echo "+++ Will install the following services: $pkgs"
+
+    echo "+++ generate hmac secret"
+    hmac=$PWD/hmac.random
+    perl -e 'open(R, "< /dev/urandom") or die; sysread(R, $K, 20) or die; print $K' > $hmac
+
+    echo
+    echo "+++ prepare secrets"
+    dbsconfig=dbsconfig.json
+    dasconfig=dasconfig.json
+    tfaasconfig=tfaas-config.json
+    httpsgoconfig=httpsgoconfig.json
+    ./make_das_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dasconfig
+    ./make_dbs_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dbssecrets
+    ./make_frontend_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
+    ./make_couchdb_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
+    ./make_reqmgr_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
+    ./make_reqmon_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
+    ./make_workqueue_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac
+    ./make_tfaas_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $tfaasconfig
+    ./make_exporters_secret.sh $robot_key $robot_crt
+    ./make_httpsgo_secret.sh $httpsgoconfig
+    ./make_dbs2go_secret.sh $robot_key $robot_crt $server_key $server_crt $hmac $dbsconfig $dbfile
+
+    echo "+++ apply secrets"
+    for p in $pkgs; do
+        kubectl apply -f ${p}-secrets.yaml --validate=false
+    done
+    rm *secrets.yaml $hmac
+
+    echo "+++ create secrets for TLS case"
+    # generate tls.key/tls.crt for custom CA
+    # openssl genrsa -out tls.key 3072 -config openssl.cnf; openssl req -new -x509 -key tls.key -sha256 -out tls.crt -days 730 -config openssl.cnf -subj "/CN=cmsweb-test.web.cern.ch"
+
+    # generate tls.key/tls.crt
+    #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=cmsweb-test.web.cern.ch"
+
+    # create secret with our tls.key/tls.crt
+    #kubectl create secret tls cluster-tls-cert --key=tls.key --cert=tls.crt
+
+    # create secret with our key/crt (they can be generated at ca.cern.ch/ca, see Host certificates)
+    echo
+    echo "+++ create cluster tls secret from key=$cmsweb_key, cert=$cmsweb_crt"
+    kubectl create secret tls cluster-tls-cert --key=$cmsweb_key --cert=$cmsweb_crt
+
+    echo
+    echo "+++ list sercres and configmap"
+    kubectl get secrets
+    kubectl -n kube-system get secrets
+    kubectl -n kube-system get configmap
+
+    echo
+    echo "+++ label node"
+    for n in `kubectl get nodes | grep -v master | awk '{print $1}'`; do
+        kubectl label node $n role=ingress --overwrite
+        kubectl get node -l role=ingress
+    done
+
+    echo
+    echo "+++ deploy services"
+    for p in $pkgs; do
+        if [ -f ${p}.yaml ]; then
+            kubectl apply -f ${p}.yaml --validate=false
+        fi
+    done
+
+    echo
+    echo "+++ deploy traefik"
+    #kubectl -n kube-system create secret generic traefik-cert --from-file=$server_crt --from-file=$server_key
+    #kubectl -n kube-system create configmap traefik-conf --from-file=traefik.toml
+    kubectl -n kube-system apply -f traefik.yaml --validate=false
+
+    echo "+++ deploy prometheus: https://devopscube.com/setup-prometheus-monitoring-on-kubernetes/"
+    if [ ! -d "kubernetes-prometheus" ]; then
+        git clone git@github.com:vkuznet/kubernetes-prometheus.git
+    fi
+
+    echo
+    echo "+++ create monitoring namespace"
+    if [ -z "`kubectl get namespaces | grep monitoring`" ]; then
+        kubectl create namespace monitoring
+    fi
+
+    echo
+    echo "+++ deploy monitoring services"
+    kubectl -n monitoring apply -f kubernetes-prometheus/config-map.yaml --validate=false
+    kubectl -n monitoring apply -f kubernetes-prometheus/prometheus-deployment.yaml --validate=false
+    kubectl -n monitoring apply -f kubernetes-prometheus/prometheus-service.yaml --validate=false
+    kubectl -n monitoring get deployments
+    kubectl -n monitoring get pods
+    prom=`kubectl -n monitoring get pods | grep prom | awk '{print $1}'`
+    echo "### we may access prometheus locally as following"
+    echo "kubectl -n monitoring port-forward $prom 8080:9090"
+    echo "### to access prometheus externally we should do the following:"
+    echo "ssh -S none -L 30000:$kubehost:30000 $USER@lxplus.cern.ch"
+}
+
+# Main routine, perform action requested on command line.
+case ${1:-status} in
+  cleanup )
+    cleanup
+    ;;
+
+  create )
+    create
+    ;;
+
+  check )
+    check
+    ;;
+
+  help )
+    perl -ne '/^##H/ && do { s/^##H ?//; print }' < $0
+    ;;
+
+  * )
+    cleanup
+    check
+    create
+    ;;
+esac
