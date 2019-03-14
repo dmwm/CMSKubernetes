eb2a45b73357fac6e957d1d6ac95a21209121aa6
Valentin <vkuznet@gmail.com>
2019-03-14 16:55:32 +0100

    remote httpsgo deployment

--- kubernetes/k8s-whoami/deploy.sh
+++ kubernetes/k8s-whoami/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cluster=whoami
+cluster=k8s-whoami
 host=`openstack --os-project-name "CMS Webtools Mig" coe cluster show $cluster | grep node_addresses | awk '{print $4}' | sed -e "s,\[u',,g" -e "s,'\],,g"`
 kubehost=`host $host | awk '{print $5}' | sed -e "s,ch.,ch,g"`
 echo "Kubernetes host: $kubehost"
@@ -11,32 +11,19 @@ clsname=`kubectl get nodes | tail -1 | awk '{print $1}'`
 kubectl label node $clsname role=ingress --overwrite
 kubectl get node -l role=ingress
 
-# prepare secrets
-httpsgoconfig=httpsgoconfig.json
-robot_key=/afs/cern.ch/user/v/valya/private/certificates/robotkey.pem
-robot_crt=/afs/cern.ch/user/v/valya/private/certificates/robotcert.pem
-#whoami_key=/afs/cern.ch/user/v/valya/private/certificates/whoami-hostkey.pem
-#whoami_crt=/afs/cern.ch/user/v/valya/private/certificates/whoami-hostcert.pem
-./make_httpsgo_secret.sh $httpsgoconfig
-
 echo "### apply secrets"
 kubectl delete secret/cluster-tls-cert
-kubectl delete secret/httpsgo-secrets
-kubectl apply -f httpsgo-secrets.yaml --validate=false
 rm *secrets.yaml
 
 echo "### create secrets for TLS case"
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=whoami.web.cern.ch"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=k8s-whoami.web.cern.ch"
 kubectl create secret tls cluster-tls-cert --key=tls.key --cert=tls.crt
-#kubectl create secret tls cluster-tls-cert --key=$whoami_key --cert=$whoami_crt
 
 echo
 echo "### delete services"
 kubectl delete -f httpgo.yaml
-kubectl delete -f httpsgo.yaml
 kubectl delete -f ing.yaml
 kubectl apply -f httpgo.yaml --validate=false
-kubectl apply -f httpsgo.yaml --validate=false
 kubectl apply -f ing.yaml --validate=false
 
 sleep 2
