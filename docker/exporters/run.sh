6d22856d24b47b5eff1b72488ac1c9df399102d2
Valentin <vkuznet@gmail.com>
2019-03-16 16:18:03 +0100

    Added new exporters

--- docker/exporters/run.sh
+++ docker/exporters/run.sh
@@ -5,8 +5,9 @@
 sleep 2
 
 # start exporter servers
-nohup $WDIR/bin/das2go_exporter -uri http://cmsweb-test.web.cern.ch:8212/status 2>&1 1>& das2go_exporter.log < /dev/null &
-nohup $WDIR/bin/reqmgr_exporter -namespace reqmgr -uri https://cmsweb-test.web.cern.ch:31443/reqmgr2/data/proc_status 2>&1 1>& reqmgr_exporter.log < /dev/null &
+nohup $WDIR/bin/das2go_exporter -address ":18217" 2>&1 1>& das2go_exporter.log < /dev/null &
+nohup $WDIR/bin/reqmgr_exporter -namespace reqmgr -uri https://localhost:8246/reqmgr2/data/proc_status 2>&1 1>& reqmgr_exporter.log < /dev/null &
+nohup $WDIR/bin/process_monitor.sh ".*DBSGlobalReader" dbs_global_exporter ":18250" 15 2>&1 1>& das2go_exporter.log < /dev/null &
 # start node exporter
 nohup $WDIR/bin/node_exporter 2>&1 1>& node_exporter.log < /dev/null &
 # start grafana server
