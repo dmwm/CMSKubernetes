44fb09cfb7497da99c7bd9a9dc10425b680fe174
Valentin <vkuznet@gmail.com>
2019-03-16 16:12:59 +0100

    Remove obsolete line

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -9,7 +9,6 @@ fi
 wdir=$PWD
 cluster=$1
 export KUBECONFIG=$wdir/$cluster/config
-#host=`openstack coe cluster show vkcluster | grep node_addresses | awk '{print $4}' | sed -e "s,\[u',,g" -e "s,'\],,g"`
 host=`openstack --os-project-name "CMS Webtools Mig" coe cluster show $cluster | grep node_addresses | awk '{print $4}' | sed -e "s,\[u',,g" -e "s,'\],,g"`
 kubehost=`host $host | awk '{print $5}' | sed -e "s,ch.,ch,g"`
 echo "Kubernetes host: $kubehost"
