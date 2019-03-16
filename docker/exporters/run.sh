95fd9770a6b8c058072b4ff5aaea45a64146d213
Valentin <vkuznet@gmail.com>
2019-03-16 19:44:46 +0100

    Fix run commands

--- docker/exporters/run.sh
+++ docker/exporters/run.sh
@@ -6,8 +6,8 @@ sleep 2
 
 # start exporter servers
 nohup $WDIR/bin/das2go_exporter -address ":18217" 2>&1 1>& das2go_exporter.log < /dev/null &
-nohup $WDIR/bin/reqmgr_exporter -namespace reqmgr -uri https://localhost:8246/reqmgr2/data/proc_status 2>&1 1>& reqmgr_exporter.log < /dev/null &
-nohup $WDIR/bin/process_monitor.sh ".*DBSGlobalReader" dbs_global_exporter ":18250" 15 2>&1 1>& das2go_exporter.log < /dev/null &
+nohup $WDIR/bin/reqmgr_exporter -namespace reqmgr -uri http://localhost:8246/reqmgr2/data/proc_status 2>&1 1>& reqmgr_exporter.log < /dev/null &
+nohup $WDIR/bin/process_monitor.sh ".*DBSGlobalReader" dbs_global_exporter ":18250" 15 2>&1 1>& dbs_exporter.log < /dev/null &
 # start node exporter
 nohup $WDIR/bin/node_exporter 2>&1 1>& node_exporter.log < /dev/null &
 # start grafana server
