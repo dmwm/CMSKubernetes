4f74568e820f1dbdf6fc56007bd06631f3bb5940
Valentin <vkuznet@gmail.com>
2019-03-18 00:03:22 +0100

    Add das2go exporter

--- docker/das2go/run.sh
+++ docker/das2go/run.sh
@@ -8,6 +8,10 @@ ls -la /data/srv/current/auth/proxy/proxy
 mongod --config $WDIR/mongodb.conf
 # upload DASMaps into MongoDB
 das_js_import /data/DASMaps/js
+
+# start das2go exporter
+nohup das2go_exporter -address ":18217" 2>&1 1>& das2go_exporter.log < /dev/null &
+
 # start das2go server
 if [ -f /etc/secrets/dasconfig.json ]; then
     das2go_monitor -config /etc/secrets/dasconfig.json
