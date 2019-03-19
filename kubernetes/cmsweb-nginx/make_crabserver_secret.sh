48359fa675f2b2576ad8c6356e6e5d4ecf57fc93
Valentin <vkuznet@gmail.com>
2019-03-19 20:56:08 +0100

    New configs for crabserver/cache services

---
+++ kubernetes/cmsweb-nginx/make_crabserver_secret.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# != 5 ]; then
+    echo "make_crabserver_secret.sh <robot_key> <robot_cert> <server_key> <server_cert> <hmac>"
+    exit 1
+fi
+rkey=`cat $1 | base64 | awk '{ORS=""; print $0}'`
+rcert=`cat $2 | base64 | awk '{ORS=""; print $0}'`
+skey=`cat $3 | base64 | awk '{ORS=""; print $0}'`
+cert=`cat $4 | base64 | awk '{ORS=""; print $0}'`
+hmac=`cat $5 | base64 | awk '{ORS=""; print $0}'`
+cat > crabserver-secrets.yaml << EOF
+apiVersion: v1
+data:
+  robotcert.pem: $rcert
+  robotkey.pem: $rkey
+  server.crt: $cert
+  server.key: $skey
+  hmac: $hmac
+kind: Secret
+metadata:
+  name: crabserver-secrets
+  namespace: default
+  selfLink: /api/v1/namespaces/default/secrets/crabserver-secrets
+type: Opaque
+EOF
