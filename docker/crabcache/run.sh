99293fa78ad8a6ed117180e85f01d5a13a33d52b
Valentin <vkuznet@gmail.com>
2019-03-19 20:58:42 +0100

    Add crabserver/cache docker scripts

---
+++ docker/crabcache/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# overwrite host PEM files in /data/srv area
+if [ -f /etc/secrets/robotkey.pem ]; then
+    sudo cp /etc/secrets/robotkey.pem /data/srv/current/auth/crabcache/dmwm-service-key.pem
+    sudo cp /etc/secrets/robotcert.pem /data/srv/current/auth/crabcache/dmwm-service-cert.pem
+fi
+
+# overwrite proxy file with one from secrets
+if [ -f /etc/secrets/proxy ]; then
+    mkdir -p /data/srv/state/crabcache/proxy
+    /bin/cp -f /etc/secrets/proxy /data/srv/state/crabcache/proxy/proxy.cert
+fi
+
+# overwrite header-auth key file with one from secrets
+if [ -f /etc/secrets/hmac ]; then
+    sudo rm /data/srv/current/auth/wmcore-auth/header-auth-key
+    cp /etc/secrets/hmac /data/srv/current/auth/wmcore-auth/header-auth-key
+fi
+
+# get proxy
+/data/proxy.sh $USER
+sleep 2
+
+# start the service
+/data/srv/current/config/crabcache/manage start 'I did read documentation'
+
+# start process exporter
+nohup process_monitor.sh ".*crabcache/config.py" crabcache_exporter ":18271" 15 2>&1 1>& crabcache_exporter.log < /dev/null &
+
+# start cron daemon
+sudo /usr/sbin/crond -n
---
+++ docker/crabserver/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# overwrite host PEM files in /data/srv area
+if [ -f /etc/secrets/robotkey.pem ]; then
+    sudo cp /etc/secrets/robotkey.pem /data/srv/current/auth/crabserver/dmwm-service-key.pem
+    sudo cp /etc/secrets/robotcert.pem /data/srv/current/auth/crabserver/dmwm-service-cert.pem
+fi
+
+# overwrite proxy file with one from secrets
+if [ -f /etc/secrets/proxy ]; then
+    mkdir -p /data/srv/state/crabserver/proxy
+    /bin/cp -f /etc/secrets/proxy /data/srv/state/crabserver/proxy/proxy.cert
+fi
+
+# overwrite header-auth key file with one from secrets
+if [ -f /etc/secrets/hmac ]; then
+    sudo rm /data/srv/current/auth/wmcore-auth/header-auth-key
+    cp /etc/secrets/hmac /data/srv/current/auth/wmcore-auth/header-auth-key
+fi
+
+# get proxy
+/data/proxy.sh $USER
+sleep 2
+
+# start the service
+/data/srv/current/config/crabserver/manage start 'I did read documentation'
+
+# start process exporter
+nohup process_monitor.sh ".*crabserver/config.py" crabserver_exporter ":18270" 15 2>&1 1>& crabserver_exporter.log < /dev/null &
+
+# start cron daemon
+sudo /usr/sbin/crond -n
