cffc993d5f864681a7d3385dc120a83b5443e980
Valentin <vkuznet@gmail.com>
2019-03-20 22:49:46 +0100

    Use process_exporter directly by determine reqmgr pid

--- docker/reqmgr/monitor.sh
+++ docker/reqmgr/monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # start process exporter
-nohup process_monitor.sh ".*wmc-httpd.*reqmgr2.*" reqmgr2_exporter ":18246" 15 2>&1 1>& reqmgr2_exporter.log < /dev/null &
-
+#nohup process_monitor.sh ".*wmc-httpd.*reqmgr2.*" reqmgr2_exporter ":18246" 15 2>&1 1>& reqmgr2_exporter.log < /dev/null &
+pid=`ps axjfwww | grep "wmc-httpd.*reqmgr2" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+prefix=reqmgr2_exporter
+address=":18246"
+nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
