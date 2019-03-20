d447dd356e58c5c13d63a782222b1f1e078d4cd3
Valentin <vkuznet@gmail.com>
2019-03-21 00:59:43 +0100

    Change port and prefix

--- docker/reqmon/monitor.sh
+++ docker/reqmon/monitor.sh
@@ -2,6 +2,6 @@
 
 # start process exporter
 pid=`ps axjfwww | grep "wmc-httpd.*reqmon" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
-prefix=reqmgr2_exporter
-address=":18246"
+prefix=reqmon_exporter
+address=":18249"
 nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
