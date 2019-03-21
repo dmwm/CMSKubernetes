0d516ebc120cfc1dd530d5e378e301e8443998c6
Valentin <vkuznet@gmail.com>
2019-03-21 15:56:40 +0100

    Add process exporter

--- docker/crabcache/monitor.sh
+++ docker/crabcache/monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# start process exporter
-nohup process_monitor.sh ".*wmc-httpd.*crabcache.*" crabcache_exporter ":18271" 15 2>&1 1>& crabcache_exporter.log < /dev/null &
-
+# start process exporter for crabcache
+pid=`ps axjfwww | grep "wmc-httpd.*crabcache" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+prefix=crabcache_exporter
+address=":18271"
+nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
