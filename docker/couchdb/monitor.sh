1e152433fe00fef081831c4315f482177da63be9
Valentin <vkuznet@gmail.com>
2019-03-21 15:56:16 +0100

    Use explicit port to allow access from outsides

--- docker/couchdb/monitor.sh
+++ docker/couchdb/monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start couchdb exporter
-nohup $GOPATH/bin/couchdb-prometheus-exporter 2>&1 1>& couchdb_exporter.log < /dev/null &
+nohup $GOPATH/bin/couchdb-prometheus-exporter -telemetry.address=":9984" 2>&1 1>& couchdb_exporter.log < /dev/null &
