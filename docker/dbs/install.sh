1661931581df2071aa037a523dc638b929102231
Valentin <vkuznet@gmail.com>
2019-03-22 19:12:05 +0100

    Extra space

--- docker/dbs/install.sh
+++ docker/dbs/install.sh
@@ -43,6 +43,7 @@ ln -s /data/srv/current/apps.sw /data/srv/current/apps
 crontab -l | \
     sed -e "s,/data/certs/hostcert.pem,/etc/secrets/proxy,g" \
         -e "s,/data/certs/hostkey.pem,/etc/secrets/proxy,g" | crontab -
+
 # add proxy generation via robot certificate
 crontab -l | egrep -v "reboot" > /tmp/mycron
 echo "3 */3 * * * sudo /data/proxy.sh $USER 2>&1 1>& /dev/null" >> /tmp/mycron
