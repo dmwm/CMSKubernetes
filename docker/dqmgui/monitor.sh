b7b4fdc1ae1ba81a0dd47d045932a85876d31fa9
Valentin <vkuznet@gmail.com>
2019-03-24 15:27:16 +0100

    New set of files for DQMGUI

---
+++ docker/dqmgui/monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# start process exporter
+#pid=`ps axjfwww | grep "wmc-httpd.*reqmon" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+#prefix=reqmon_exporter
+#address=":18249"
+#nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
