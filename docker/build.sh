60cef7840a2d85d3f5d6593d20f4970f5a92ecbb
Valentin <vkuznet@gmail.com>
2019-03-25 15:51:17 +0100

    re-organize build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,69 +1,21 @@
 #!/bin/bash
 echo "prune all images"
-#docker system prune -f -a
-echo "### build cmssw/cmsweb"
-docker build -t cmssw/cmsweb cmsweb
-docker push cmssw/cmsweb
+echo "docker system prune -f -a"
 
-echo "### build cmssw/frontend"
-docker build -t cmssw/frontend frontend
-docker push cmssw/frontend
-
-echo "### build cmssw/exporters"
-docker build -t cmssw/exporters exporters
-docker push cmssw/exporters
-
-echo "### build cmssw/das2go"
-docker build -t cmssw/das2go das2go
-docker push cmssw/das2go
-
-echo "### build cmssw/dbs2go"
-docker build -t cmssw/dbs2go dbs2go
-docker push cmssw/dbs2go
-
-echo "### build cmssw/dbs"
-docker build -t cmssw/dbs dbs
-docker push cmssw/dbs
-
-echo "### build cmssw/couchdb"
-docker build -t cmssw/couchdb couchdb
-docker push cmssw/couchdb
-
-echo "### build cmssw/reqmgr"
-docker build -t cmssw/reqmgr reqmgr
-docker push cmssw/reqmgr
-
-echo "### build cmssw/reqmon"
-docker build -t cmssw/reqmon reqmon
-docker push cmssw/reqmon
-
-echo "### build cmssw/workqueue"
-docker build -t cmssw/workqueue workqueue
-docker push cmssw/workqueue
-
-echo "### build veknet/crabserver"
-docker build -t veknet/crabserver crabserver
-docker push veknet/crabserver
-
-echo "### build veknet/crabcache"
-docker build -t veknet/crabcache crabcache
-docker push veknet/crabcache
-
-echo "### build veknet/dqmgui"
-docker build -t veknet/dqmgui dqmgui
-docker push veknet/dqmgui
-
-echo "### build veknet/httpgo"
-docker build -t veknet/httpgo httpgo
-docker push veknet/httpgo
-
-echo "### build veknet/httpsgo"
-docker build -t veknet/httpsgo httpsgo
-docker push veknet/httpsgo
-
-#echo "### build veknet/tfaas"
-#docker build -t veknet/tfaas tfaas
-#docker push veknet/tfaas
+cmssw_pkgs="cmsweb frontend exporters das2go dbs2go dbs couchdb reqmgr reqmon workqueue"
+repo=cmssw
+for pkg in $cmssw_pkgs; do
+    echo "### build $repo/$pkg"
+    docker build -t $repo/$pkg $pkg
+    docker push $repo/$pkg
+done
+priv_pkgs="dbsmigration crabserver crabcache dqmgui t0_reqmon t0wmadatasvc httpgo httpsgo tfaas"
+repo=veknet
+for pkg in $priv_pkgs; do
+    echo "### build $repo/$pkg"
+    docker build -t $repo/$pkg $pkg
+    docker push $repo/$pkg
+done
 
 echo
 echo "To remove all images please use this command"
