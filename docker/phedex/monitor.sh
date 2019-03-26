079f1df219aa0abee436b5818c4929affd8997d5
Valentin <vkuznet@gmail.com>
2019-03-26 16:12:10 +0100

    Change pattern

--- docker/phedex/monitor.sh
+++ docker/phedex/monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # start process exporter
-pid=`ps axjfwww | grep "wmc-httpd.*phedex" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+pid=`ps axjfwww | grep "phedex-web.py" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
 prefix=phedex_exporter
 address=":17001"
 nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
