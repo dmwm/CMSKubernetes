114e465913f91ad1de114a2221247f69e64c46ff
Valentin <vkuznet@gmail.com>
2019-04-04 19:02:54 +0200

    Complete all cmsweb images to push to cmssw

--- docker/build.sh
+++ docker/build.sh
@@ -2,16 +2,9 @@
 echo "prune all images"
 echo "docker system prune -f -a"
 
-cmssw_pkgs="cmsweb frontend exporters das2go dbs2go dbs couchdb reqmgr reqmon workqueue acdcserver alertscollector confdb crabserver crabcache dmwmmon dqmgui t0_reqmon t0wmadatasvc dbsmigration"
+cmssw_pkgs="cmsweb frontend exporters das2go dbs2go dbs couchdb reqmgr reqmgr2ms reqmon workqueue acdcserver alertscollector confdb crabserver crabcache cmsmon dmwmmon dqmgui t0_reqmon t0wmadatasvc dbsmigration phedex sitedb httpgo httpsgo tfaas"
 repo=cmssw
 for pkg in $cmssw_pkgs; do
-    echo "### build $repo/$pkg"
-    docker build -t $repo/$pkg $pkg
-    docker push $repo/$pkg
-done
-priv_pkgs="httpgo httpsgo tfaas phedex sitedb"
-repo=veknet
-for pkg in $priv_pkgs; do
     echo "### build $repo/$pkg"
     docker build -t $repo/$pkg $pkg
     docker push $repo/$pkg
@@ -20,3 +13,4 @@ done
 echo
 echo "To remove all images please use this command"
 echo "docker rmi \$(docker images -qf \"dangling=true\")"
+echo "docker images | awk '{print \"docker rmi -f \"$3\"\"}' | /bin/sh"
