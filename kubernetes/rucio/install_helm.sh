c713fd23d11065e08078987ecb5d4fdda5bedd4b
Eric Vaandering <ericvaandering@gmail.com>
2019-04-05 16:23:22 +0200

    Fix server to work with port 443, work with nginx including auth server
    Add scripts for secrets, helm

---
+++ kubernetes/rucio/install_helm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install helm into a newly created cluster
+
+helm init --upgrade
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}’
+
+kubectl get pods --all-namespaces
--- kubernetes/rucio/install_rucio_bleeding.sh
+++ kubernetes/rucio/install_rucio_bleeding.sh
@@ -1,15 +1,22 @@
 #! /bin/sh
+SERVER_NAME=cms-rucio-testbed
+DAEMON_NAME=cms-ruciod-testbed
+
+# Ingress server
+
+helm install stable/nginx-ingress --namespace kube-system --name ingress-nginx --set rbac.create=true --values nginx-ingress.yaml
 
 # Rucio server, daemons, and daemons for analysis
 
-helm install --name cms-rucio-testbed --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-server-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-server
-helm install --name cms-ruciod-testbed --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-daemons
-helm install --name cms-analysisd-testbed --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-daemons
+helm install --name $SERVER_NAME --values cms-rucio-common.yaml,cms-rucio-server-nginx.yaml,nginxtest-rucio-server.yaml,cms-rucio-dev-db.yaml ~/rucio-helm-charts/rucio-server
+helm install --name $DAEMON_NAME --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-dev-db.yaml ~/rucio-helm-charts/rucio-daemons
 
 # Graphite and other services
-helm install --name graphite --values rucio-graphite.yaml,rucio-graphite-ingress.yaml,rucio-graphite-pvc.yaml kiwigrid/graphite
+helm install --name graphite --values rucio-graphite.yaml,rucio-graphite-nginx.yaml,rucio-graphite-pvc.yaml kiwigrid/graphite
 
 # Filebeat and logstash
 helm install --name logstash --values cms-rucio-logstash.yml,logstash-filter-oldtest.yml stable/logstash
 helm install --name filebeat --values cms-rucio-filebeat.yml  stable/filebeat
 
+kubectl delete job fts
+kubectl create job --from=cronjob/${DAEMON_NAME}-renew-fts-proxy fts
