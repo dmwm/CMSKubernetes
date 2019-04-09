53573565211fb8a2f15675c16d1c959b6f8074b0
Eric Vaandering <ericvaandering@gmail.com>
2019-04-09 11:03:46 -0500

    Add scripts for secrets, helm

---
+++ kubernetes/rucio/create_secrets.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This script will create the various secrets needed by our installation. Before running set the following env variables
+
+# HOSTP12 - The .p12 file from corresponding to the host certificate
+# ROBOTCERT - The robot certificate (named usercert.pem)
+# ROBOTKEY  - The robot certificate key (unencrypted, named new_userkey.pem)
+
+echo "When prompted, enter the password used to encrypt the P12 file"
+
+# Secret for redirecting server traffic from 443 to 80
+openssl pkcs12 -in ~/.globus/Rucio/nginxtest4-4epk5oj55wka-minion-0.p12 -clcerts -nokeys -out ./tls.crt
+openssl pkcs12 -in ~/.globus/Rucio/nginxtest4-4epk5oj55wka-minion-0.p12  -nocerts -nodes -out ./tls.key
+kubectl create secret tls rucio-server.tls-secret --key=tls.key --cert=tls.crt
+
+# Secrets for the auth server
+cp tls.key hostkey.pem
+cp tls.cert hostcert.pem
+cp /etc/pki/tls/certs/CERN_Root_CA.pem ca.pem
+
+kubectl create secret generic host-cert --from-file=hostcert.pem
+kubectl create secret generic host-key --from-file=hostkey.pem
+kubectl create secret generic ca  --from-file=ca.pem
+
+# Clean up
+rm tls.key tls.cert hostkey.pem hostcert.pem ca.pem
+
+# Secrets for FTS, hermes
+
+kubectl create secret generic fts-cert --from-file=$ROBOTCERT
+kubectl create secret generic fts-key --from-file=$ROBOTKEY
+kubectl create secret generic hermes-cert --from-file=$ROBOTCERT
+kubectl create secret generic hermes-key --from-file=$ROBOTKEY
+kubectl create secret generic cms-ruciod-testbed-rucio-ca-bundle --from-file=/etc/pki/tls/certs/CERN-bundle.pem
+
+
+kubectl get secrets
--- kubernetes/rucio/install_rucio_bleeding.sh
+++ kubernetes/rucio/install_rucio_bleeding.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+# Ingress server
+
+helm install stable/nginx-ingress --namespace kube-system --name ingress-nginx --set rbac.create=true --values nginx-ingress.yaml
+
 # Rucio server, daemons, and daemons for analysis
 
 helm install --name cms-rucio-testbed --values cms-rucio-common.yaml,cms-rucio-server.yaml,cms-rucio-server-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-server
 helm install --name cms-ruciod-testbed --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-daemons
-helm install --name cms-analysisd-testbed --values cms-rucio-common.yaml,cms-rucio-analysis-daemons.yaml,cms-rucio-daemons-oldtest.yaml,cms-rucio-oldtest-db.yaml rucio/rucio-daemons
 
 # Graphite and other services
 helm install --name graphite --values rucio-graphite.yaml,rucio-graphite-ingress.yaml,rucio-graphite-pvc.yaml kiwigrid/graphite
