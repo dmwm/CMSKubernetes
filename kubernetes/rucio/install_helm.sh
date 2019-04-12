fcbaa45f18023d0b8d7f5f5020f8bb253847156d
Eric Vaandering <ericvaandering@gmail.com>
2019-04-12 18:15:14 +0200

    Add the integration server
    And remove some scripts

--- kubernetes/rucio/install_helm.sh
+++ kubernetes/rucio/install_helm.sh
@@ -5,6 +5,6 @@
 helm init --upgrade
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}’
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
 kubectl get pods --all-namespaces
