5a47e9efa467aaf66d7eba3bff8f2cda8d580e19
Nick Smith <nick.smith@cern.ch>
2019-05-20 21:16:44 +0200

    No pvc in upgrade script

--- kubernetes/rucio/upgrade_rucio_dev.sh
+++ kubernetes/rucio/upgrade_rucio_dev.sh
@@ -5,12 +5,11 @@ REPO=~/rucio-helm-charts # or rucio
 SERVER_NAME=cms-rucio-dev
 DAEMON_NAME=cms-ruciod-dev
 
-#helm upgrade --values rucio-graphite.yaml  graphite kiwigrid/graphite # Don't do PVC again
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-server-nginx.yaml,dev-rucio-server.yaml,dev-db.yaml,dev-release.yaml  $SERVER_NAME $REPO/rucio-server
 helm upgrade --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,dev-rucio-daemons.yaml,dev-db.yaml,dev-release.yaml $DAEMON_NAME $REPO/rucio-daemons
 
 # Graphite and other services (currently not doing anything with them)
-helm upgrade --values rucio-graphite.yaml,rucio-graphite-nginx.yaml,rucio-graphite-pvc.yaml,dev-graphite.yaml graphite kiwigrid/graphite
+helm upgrade --values rucio-graphite.yaml,rucio-graphite-nginx.yaml,dev-graphite.yaml graphite kiwigrid/graphite
 # helm install --name grafana --values rucio-grafana.yaml,dev-grafana.yaml stable/grafana
 # kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode > dev_grafana_password.txt
 
