d6b73d59fa258e1939257e7b64337b307c93f7c7
Valentin <vkuznet@gmail.com>
2019-06-25 20:20:21 +0200

    Rename reqmgr to reqmgr2

--- kubernetes/cmsweb-nginx/make_reqmgr2_secret.sh
+++ kubernetes/cmsweb-nginx/make_reqmgr2_secret.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ $# != 3 ]; then
-    echo "make_reqmgr_secret.sh <robot_key> <robot_cert> <hmac>"
+    echo "make_reqmgr2_secret.sh <robot_key> <robot_cert> <hmac>"
     exit 1
 fi
 rkey=`cat $1 | base64 | awk '{ORS=""; print $0}'`
 rcert=`cat $2 | base64 | awk '{ORS=""; print $0}'`
 hmac=`cat $3 | base64 | awk '{ORS=""; print $0}'`
-cat > reqmgr-secrets.yaml << EOF
+cat > reqmgr2-secrets.yaml << EOF
 apiVersion: v1
 data:
   robotcert.pem: $rcert
@@ -14,8 +14,8 @@ data:
   hmac: $hmac
 kind: Secret
 metadata:
-  name: reqmgr-secrets
+  name: reqmgr2-secrets
   namespace: default
-  selfLink: /api/v1/namespaces/default/secrets/reqmgr-secrets
+  selfLink: /api/v1/namespaces/default/secrets/reqmgr2-secrets
 type: Opaque
 EOF
