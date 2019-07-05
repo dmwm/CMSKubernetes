6cee653381f68a4c980d959e62b39e56ca00a9d0
Valentin <vkuznet@gmail.com>
2019-07-05 21:04:26 +0200

    Add proxy package; add removal of all images once they are uploaded

--- docker/build.sh
+++ docker/build.sh
@@ -2,12 +2,17 @@
 echo "prune all images"
 echo "docker system prune -f -a"
 
-cmssw_pkgs="cmsweb frontend exporters das2go dbs2go dbs couchdb reqmgr reqmgr2ms reqmon workqueue acdcserver alertscollector confdb crabserver crabcache cmsmon dmwmmon dqmgui t0_reqmon t0wmadatasvc dbsmigration phedex sitedb httpgo httpsgo tfaas"
+cmssw_pkgs="cmsweb proxy frontend exporters das dbs2go dbs couchdb reqmgr reqmgr2ms reqmon workqueue acdcserver alertscollector confdb crabserver crabcache cmsmon dmwmmon dqmgui t0_reqmon t0wmadatasvc dbsmigration phedex sitedb httpgo httpsgo tfaas"
 repo=cmssw
 for pkg in $cmssw_pkgs; do
     echo "### build $repo/$pkg"
     docker build -t $repo/$pkg $pkg
+    echo "### existing images"
+    docker images
     docker push $repo/$pkg
+    if [ "$pkg" != "cmsweb" ]; then
+        docker rmi $repo/$pkg
+    fi
 done
 
 echo
