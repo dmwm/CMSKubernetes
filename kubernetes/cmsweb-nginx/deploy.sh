5d75861d6424badb257d38d6c0889167905cfc09
Valentin <vkuznet@gmail.com>
2019-07-06 20:26:17 +0200

    comment out small set of packages

--- kubernetes/cmsweb-nginx/deploy.sh
+++ kubernetes/cmsweb-nginx/deploy.sh
@@ -205,7 +205,7 @@ create()
 {
     # adjust as necessary
     pkgs="ing-nginx proxy-account proxy-cron frontend acdcserver alertscollector cmsmon confdb couchdb crabcache crabserver das dbs dbsmigration dmwmmon dqmgui exporters httpgo httpsgo phedex reqmgr2 reqmgr2ms reqmon sitedb t0_reqmon t0wmadatasvc workqueue"
-    pkgs="ing-nginx proxy-account proxy-cron frontend dbs das couchdb reqmgr2 reqmon workqueue crabcache crabserver dqmgui dmwmmon"
+#    pkgs="ing-nginx proxy-account proxy-cron frontend dbs das couchdb reqmgr2 reqmon workqueue crabcache crabserver dqmgui dmwmmon"
 
     echo "### CREATE ACTION ###"
     echo "+++ install services: $pkgs"
