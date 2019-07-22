7ddc9bde7cae3780c7d5f473ac108c06a524b583
Valentin <vkuznet@gmail.com>
2019-07-22 21:42:52 +0200

    Rewrite das using cmsweb deployment procedure

---
+++ docker/das/install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+ARCH=slc7_amd64_gcc630
+VER=HG1907f
+REPO="comp"
+AREA=/data/cfg/admin
+PKGS="admin backend das mongodb"
+SERVER=cmsrep.cern.ch
+
+cd $WDIR
+git clone git://github.com/dmwm/deployment.git cfg
+mkdir $WDIR/srv
+
+cd $WDIR/cfg
+git reset --hard $VER
+
+# adjust deploy script to use k8s host name
+cmsk8s_prod=${CMSK8S:-https://cmsweb.cern.ch}
+cmsk8s_prep=${CMSK8S:-https://cmsweb-testbed.cern.ch}
+cmsk8s_dev=${CMSK8S:-https://cmsweb-dev.cern.ch}
+cmsk8s_priv=${CMSK8S:-https://cmsweb-test.web.cern.ch}
+sed -i -e "s,https://cmsweb.cern.ch,$cmsk8s_prod,g" \
+    -e "s,https://cmsweb-testbed.cern.ch,$cmsk8s_prep,g" \
+    -e "s,https://cmsweb-dev.cern.ch,$cmsk8s_dev,g" \
+    -e "s,https://\`hostname -f\`,$cmsk8s_priv,g" \
+    das/deploy
+
+# create fake certs since we'll be using them at deployment time
+mkdir -p /data/certs
+touch /data/certs/hostcert.pem
+touch /data/certs/hostkey.pem
+
+# Deploy services
+# we do not use InstallDev script directly since we want to capture the status of
+# install step script. Therefore we call Deploy script and capture its status every step
+cd $WDIR
+curl -sO http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
+sh -x ./bootstrap.sh -architecture $ARCH -path $WDIR/tmp/$VER/sw -repository $REPO -server $SERVER setup
+# deploy services
+$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s prep $WDIR/srv "$PKGS"
+if [ $? -ne 0 ]; then
+    cat $WDIR/srv/.deploy/*-prep.log
+    exit 1
+fi
+$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s sw $WDIR/srv "$PKGS"
+if [ $? -ne 0 ]; then
+    cat $WDIR/srv/.deploy/*-sw.log
+    exit 1
+fi
+
+# make fake proxy to allow post install step to succeed
+mkdir -p /data/srv/$VER/auth/proxy
+rm /data/srv/$VER/auth/proxy/*
+
+$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s post $WDIR/srv "$PKGS"
+if [ $? -ne 0 ]; then
+    cat $WDIR/srv/.deploy/*-post.log
+    exit 1
+fi
+
+# comment out usage of port 8443 in k8s setup
+files=`find /data/srv/$VER/sw/$ARCH -type f | xargs grep ":8443" | awk '{print $1}' | sed -e "s,:,,g" | grep py$`
+for fname in $files; do
+    sed -i -e "s,:8443,,g" $fname
+done
+
+# Adjust ServerMonitor to be specific
+sed -i -e "s#ServerMonitor/2.0#ServerMonitor-das#g" /data/srv/current/config/admin/ServerMonitor
+
+# adjust crontab
+crontab -l | egrep -v "reboot|ProxyRenew" > /tmp/mycron
+crontab /tmp/mycron
+rm /tmp/mycron
--- docker/das/run.sh
+++ docker/das/run.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
+
+# overwrite host PEM files in /data/srv area
+if [ -f /etc/secrets/robotkey.pem ]; then
+    sudo cp /etc/secrets/robotkey.pem /data/srv/current/auth/das/dmwm-service-key.pem
+    sudo cp /etc/secrets/robotcert.pem /data/srv/current/auth/das/dmwm-service-cert.pem
+fi
+
 # overwrite proxy if it is present in /etc/proxy
 if [ -f /etc/proxy/proxy ]; then
     mkdir -p /data/srv/state/das/proxy
     ln -s /etc/proxy/proxy /data/srv/state/das/proxy/proxy.cert
     mkdir -p /data/srv/current/auth/proxy
     ln -s /etc/proxy/proxy /data/srv/current/auth/proxy/proxy
+fi
+
+# overwrite header-auth key file with one from secrets
+if [ -f /etc/secrets/hmac ]; then
+    sudo rm /data/srv/current/auth/wmcore-auth/header-auth-key
+    cp /etc/secrets/hmac /data/srv/current/auth/wmcore-auth/header-auth-key
+    # generate new hmac key for couch
+    chmod u+w /data/srv/current/auth/couchdb/hmackey.ini
+    perl -e 'undef $/; print "[couch_cms_auth]\n"; print "hmac_secret = ", unpack("h*", <STDIN>), "\n"' < /etc/secrets/hmac > /data/srv/current/auth/couchdb/hmackey.ini
+fi
+
+# we need to populate das dbs into couch first
+if [ -f /data/srv/state/couchdb/stagingarea/das ]; then
+    arch=`ls /data/srv/current/sw | grep bootstrap | grep log | sed -e "s,bootstrap-,,g" -e "s,.log,,g"`
+    ver=`ls /data/srv/current/sw/$arch/external/couchapp/ | tail -1`
+    source /data/srv/current/sw/$arch/external/couchapp/$ver/etc/profile.d/init.sh
+    source /data/srv/state/couchdb/stagingarea/das
 fi
 
 # use service configuration files from /etc/secrets if they are present
@@ -19,19 +43,9 @@ for fname in $files; do
     fi
 done
 
-# start mongodb
-mongod --config $WDIR/mongodb.conf
-# upload DASMaps into MongoDB
-das_js_import /data/DASMaps/js
+# start the service
+/data/srv/current/config/mongodb/manage start 'I did read documentation'
+/data/srv/current/config/das/manage start 'I did read documentation'
 
-# start monitoring
-if [ -f /data/monitor.sh ]; then
-    /data/monitor.sh
-fi
-
-# start das2go server
-if [ -f /etc/secrets/dasconfig.json ]; then
-    das2go_monitor -config /etc/secrets/dasconfig.json
-else
-    das2go_monitor -config $GOPATH/src/github.com/dmwm/das2go/dasconfig.json
-fi
+# start cron daemon
+sudo /usr/sbin/crond -n
