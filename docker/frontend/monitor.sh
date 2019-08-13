e75dc5ee1a43da71b48d98ff4428f2397036fd8a
Valentin <vkuznet@gmail.com>
2019-08-13 23:05:00 +0200

    Add filebeat

--- docker/frontend/monitor.sh
+++ docker/frontend/monitor.sh
@@ -6,8 +6,15 @@ suri="http://localhost:$aport/server-status/?auto"
 echo "Start apache_exporter with $suri"
 nohup apache_exporter -scrape_uri $suri -telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
 
-# run logstash
-if [ -f /etc/secrets/logstash.conf ] && [ -f /usr/share/logstash/bin/logstash ]; then
+# run filebeat if present (first choice)
+if [ -f /etc/secrets/filebeat.conf ] && [ -f /usr/bin/filebeat ]; then
+    ldir=/data/srv/logs/frontend/filebeat
+    mkdir -p $ldir/data
+    nohup /usr/bin/filebeat \
+        -c /etc/secrets/filebeat.conf \
+        --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
+# run logstash if present (second choice)
+elif [ -f /etc/secrets/logstash.conf ] && [ -f /usr/share/logstash/bin/logstash ]; then
     ldir=/data/srv/logs/frontend/logstash
     mkdir -p $ldir/data
     nohup /usr/share/logstash/bin/logstash \
