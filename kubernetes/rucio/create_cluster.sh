47e9c29bae52852b75ebc20b5fe229527ff118da
Eric Vaandering <ericvaandering@gmail.com>
2019-09-05 16:58:59 +0200

    Paremeterize and modernize for testbed

--- kubernetes/rucio/create_cluster.sh
+++ kubernetes/rucio/create_cluster.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-export $NAME=ewvtest
-export $MINIONS=2
+export NAME=cmsruciosync
+export MINIONS=2
+export MINIONSIZE=m2.large
 
-openstack coe cluster create $NAME --keypair lxplus --os-project-name CMSRucio --node-count $MINIONS --cluster-template kubernetes-1.14.1-1 --labels cern_enabled=True,kube_tag=v1.14.1,kube_csi_enabled=True,kube_csi_version=v0.3.2,container_infra_prefix=gitlab-registry.cern.ch/cloud/atomic-system-containers/,cvmfs_tag=qa,ceph_csi_enabled=True,flannel_backend=vxlan,ingress_controller=nginx,cern_tag=qa,tiller_enabled=true,manila_enabled=True,manila_version=v0.3.0,heat_container_agent_tag=stein-dev-1 --master-flavor m2.small    
+#openstack coe cluster create $NAME --keypair lxplus --os-project-name CMSRucio --node-count $MINIONS --cluster-template kubernetes-1.14.6-1 --labels admission_control_list="NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota,Priority",cern_enabled=True,kube_tag=v1.14.6,kube_csi_enabled=True,kube_csi_version=cern-csi-1.0-1,container_infra_prefix=gitlab-registry.cern.ch/cloud/atomic-system-containers/,cvmfs_tag=qa,cvmfs_csi_enabled=False,ceph_csi_enabled=True,flannel_backend=vxlan,ingress_controller=nginx,cern_tag=qa,tiller_enabled=true,manila_enabled=True,manila_version=v0.3.0,heat_container_agent_tag=stein-dev-1 --master-flavor m2.small  --flavor $MINIONSIZE  
+openstack coe cluster create $NAME --keypair lxplus --os-project-name CMSRucio --node-count $MINIONS --cluster-template kubernetes-1.14.6-1 --labels ingress_controller=nginx,cern_enabled=True --master-flavor m2.small  --flavor $MINIONSIZE  
 
 
 
--- kubernetes/rucio/install_rucio_testbed.sh
+++ kubernetes/rucio/install_rucio_testbed.sh
@@ -1,42 +1,8 @@
 #! /bin/sh
 
-REPO=~/rucio-helm-charts # or rucio
+export INSTANCE=testbed
+export REPO=rucio # or ~/rucio-helm-charts as an override
+export REPO=~/rucio-helm-charts # as an override
 
-SERVER_NAME=cms-rucio-testbed
-DAEMON_NAME=cms-ruciod-testbed
-UI_NAME=cms-webui-testbed
+./install_rucio.sh
 
-# Ingress server
-
-helm install stable/nginx-ingress --namespace kube-system --name ingress-nginx --values nginx-ingress.yaml
-
-# Rucio server, daemons, and daemons for analysis
-
-helm install --name $SERVER_NAME --values cms-rucio-common.yaml,cms-rucio-server.yaml,testbed-rucio-server.yaml,testbed-db.yaml,testbed-release.yaml $REPO/rucio-server
-helm install --name $DAEMON_NAME --values cms-rucio-common.yaml,cms-rucio-daemons.yaml,testbed-rucio-daemons.yaml,testbed-db.yaml,testbed-release.yaml $REPO/rucio-daemons
-helm install --name $UI_NAME --values cms-rucio-common.yaml,cms-rucio-webui.yaml,testbed-rucio-webui.yaml,testbed-db.yaml,testbed-release.yaml $REPO/rucio-ui
-
-# Graphite and other services
-helm install --name graphite --values rucio-graphite.yaml,rucio-graphite-nginx.yaml,rucio-graphite-pvc.yaml,testbed-graphite.yaml kiwigrid/graphite
-
-# Filebeat and logstash
-helm install --name logstash --values cms-rucio-logstash.yml,testbed-logstash-filter.yaml stable/logstash
-helm install --name filebeat --values cms-rucio-filebeat.yml  stable/filebeat
-
-kubectl delete job --ignore-not-found=true fts
-kubectl create job --from=cronjob/${DAEMON_NAME}-renew-fts-proxy fts
-
-# Label is key to prevent it from also syncing datasets
-kubectl apply -f testbed-sync-jobs.yaml -l syncs=rses
-
-# Set up landb loadbalance
-numberIngressNodes=3
-n=0
-kubectl get node -o name | while read node; do
-  [[ $((n++)) == $numberIngressNodes ]] && break
-  kubectl label node ${node##node/} role=ingress
-  cnames="cms-rucio-graphite-testbed--load-${n}-,cms-rucio-testbed--load-${n}-,cms-rucio-auth-testbed--load-${n}-,cms-rucio-webui-testbed--load-${ndns}-"
-  openstack server set --os-project-name CMSRucio --property landb-alias=$cnames ${node##node/}
-  # teardown:
-  # openstack server unset --os-project-name CMSRucio --property landb-alias ${node##node/}
-done
