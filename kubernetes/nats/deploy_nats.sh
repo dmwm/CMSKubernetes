177093d65d6ef5b4a1121544c7dcb1b2e53b3e0e
Valentin <vkuznet@gmail.com>
2019-11-04 23:02:40 +0100

    Update nats deployment

--- kubernetes/nats/deploy_nats.sh
+++ kubernetes/nats/deploy_nats.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
-echo "Label minion nodes with ingress label"
-kubectl get nodes | grep minion | awk '{print "kubectl label node "$1" role=ingress --overwrite"}' | /bin/sh
-
 echo "Add ingress controller"
 kubectl get nodes
-node=`kubectl get nodes | grep minion | head -1 | awk '{print $1}'`
 
-cat <<EOF | kubectl create -f -
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  name: ing-nats
-  namespace: default
-  annotations:
-    kubernetes.io/ingress.class: nginx
-spec:
-  rules:
-  - host: ${node}.cern.ch
-    http:
-      paths:
-      - path: /nats
-        backend:
-          serviceName: nats-cluster
-          servicePort: 4222
-EOF
+echo "Initialize cms-nats alias for our minions"
+kubectl get nodes | grep minion | awk 'BEGIN{i=0}{print "openstack server set --property landb-alias=cms-nats--load-"i"- "$1""; i++}'
+
+echo "Wait for cms-nats landb entry to appear..., press CTRL+C when you see them"
+watch -d host cms-nats.cern.ch
 
 echo "Deploy NATS"
 kubectl apply -f https://github.com/nats-io/nats-operator/releases/latest/download/00-prereqs.yaml
@@ -38,9 +21,3 @@ kubectl apply -f nats-cluster.yaml --validate=false
 
 echo "Now let's see if nats-cluster are Running..., press CTRL+C when you see them"
 watch -d kubectl get nats --all-namespaces
-
-echo "Let's test NATS server, press CTRL+C"
-kubectl get ing | grep ing-nats | awk '{print "curl http://"$2"/nats"}' | /bin/sh
-
-echo "We can adjust further our setup"
-kubectl get nodes | grep minion | awk 'BEGIN{i=0}{print "openstack server set --property landb-alias=cms-nats--load-"i"- "$1""; i++}'
