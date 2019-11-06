77b5d6ca4491aad3ae78939975e0107d03cc402f
Eric Vaandering <ericvaandering@gmail.com>
2019-11-06 17:02:35 +0100

    Latest probes

--- kubernetes/rucio/install_probes.sh
+++ kubernetes/rucio/install_probes.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-export CMS_REPO=~/CMSKubernetes/helm
+export REPO=~/rucio-helm-charts
 export INSTANCE=dev
-export PROBE_NAME=cms-probe-${INSTANCE}
+export PROBE_NAME=rucio-probes-${INSTANCE}
 
-helm install --name $PROBE_NAME --values cms-rucio-common.yaml,${INSTANCE}-db.yaml,dp-release.yaml $CMS_REPO/rucio-cron-jobs
+helm install --name $PROBE_NAME --values cms-rucio-common.yaml,${INSTANCE}-db.yaml,dp-release.yaml $REPO/rucio-probes
 
 
 
