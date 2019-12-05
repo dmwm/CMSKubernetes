fa76c4100f079f1cbbfac8557fae7012299f031a
Valentin <vkuznet@gmail.com>
2019-12-05 20:29:55 +0100

    Check existance of object before deleteing it

---
+++ kubernetes/prometheus/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# deploy prometheus CRD
+if [ -z `kubectl get crd | grep prometheus` ]; then
+    kubectl delete -f bundle.yaml
+fi
+kubectl apply -f bundle.yaml
+# deploy prometheus configuration
+if [ -z `kubectl get secrets | grep prometheus-config` ]; then
+    kubectl delete secret prometheus-config
+fi
+kubectl create secret generic prometheus-config --from-file=prometheus-config.yaml
+# deploy prometheus
+if [ -z `kubectl get pod | grep prometheus-prometheus` ]; then
+    kubectl delete -f prometheus.yaml
+fi
+kubectl apply -f prometheus.yaml
+# deploy pushgateway
+if [ -z `kubectl get pod | grep pushgateway` ]; then
+    kubectl delete -f pushgateway.yaml
+fi
+kubectl apply -f pushgateway.yaml
