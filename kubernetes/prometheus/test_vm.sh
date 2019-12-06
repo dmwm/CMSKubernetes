5431b5ed5d8bf5484915523f72e356e3c32c01ba
Valentin <vkuznet@gmail.com>
2019-12-06 15:20:55 +0100

    change from prometheus operator to custom deployment

---
+++ kubernetes/prometheus/test_vm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+url="http://cms-prometheus.cern.ch"
+purl=${url}:30422/api/put
+rurl=${url}:30428/api/v1/export
+echo "put data into $purl"
+curl -H 'Content-Type: application/json' -d '{"metric":"cms.dbs.exitCode", "value":8021, "tags":{"site":"T2_US", "task":"test", "log":"/path/file.log"}}' "$purl"
+echo "get data from $rurl"
+curl -G "$rurl" -d 'match[]=cms.dbs.exitCode'
