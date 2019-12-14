6c3bca9c0e7f40e4492be5a5a2c7b8f8a564b652
Valentin <vkuznet@gmail.com>
2019-12-15 00:44:24 +0100

    Adjust tools

--- kubernetes/cmsweb/scripts/test_k8s.sh
+++ kubernetes/cmsweb/scripts/test_k8s.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-##H Usage: bench_k8s.sh <url> <dbs_instance=int>
-##H
-##H Environments:
-##H CMSK8S       defines default url (cmsweb-test.cern.ch)
-##H SCRAM_ARCH   defines default CMSSW architecture( slc7_amd64_gcc700)
+##H Usage: test_k8s.sh <url> <dbs_instance=int>
 ##H
 
 if [ "$1" == "-h" ] || [ "$1" == "-help" ] || [ "$1" == "--help" ] || [ "$1" == "help" ] || [ "$1" == "" ]; then
     perl -ne '/^##H/ && do { s/^##H ?//; print }' < $0
     exit 1
-fi
-
-# define list of urls to test
-url=${CMSK8S:-https://cmsweb-test.cern.ch}
-if [ -n "$1" ]; then
-    url=$1
 fi
 
 # dbs instance, default is preproduction dbs instance
