b4ff110b47c5e258f4cfbbd2d8302eda36f80c38
Valentin <vkuznet@gmail.com>
2019-12-17 13:56:37 +0100

    Fix options

--- docker/frontend/monitor.sh
+++ docker/frontend/monitor.sh
@@ -4,7 +4,7 @@
 aport=`cat /data/srv/state/frontend/server.conf | grep Listen | grep 80 | awk '{print $2}'`
 suri="http://localhost:$aport/server-status/?auto"
 echo "Start apache_exporter with $suri"
-nohup apache_exporter -scrape_uri $suri -telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
+nohup apache_exporter --scrape_uri $suri --telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
 
 # run filebeat
 if [ -f /etc/secrets/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
