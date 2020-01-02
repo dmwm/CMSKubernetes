81048a97880fc63878616ddb246aa68a0204965c
Valentin <vkuznet@gmail.com>
2020-01-02 17:00:57 +0100

    Add POST request support and log dump of incoming requests

--- docker/httpgo/httpgo.go
+++ docker/httpgo/httpgo.go
@@ -2,29 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Println("TLS:", r.TLS)
-	fmt.Println("Header:", r.Header)
+	requestDump, err := httputil.DumpRequest(r, true)
+	if r.Method == "GET" {
+		fmt.Println("TLS:", r.TLS)
+		fmt.Println("Header:", r.Header)
 
-	// print out all request headers
-	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-	fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q\n", r.Header["Accept"])
+		// print out all request headers
+		fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+		}
+		fmt.Fprintf(w, "Host = %q\n", r.Host)
+		fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
+		fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q\n", r.Header["Accept"])
 
-	w.WriteHeader(http.StatusOK)
-	page := "Hello Go world!!!"
-	w.Write([]byte(page))
+		w.WriteHeader(http.StatusOK)
+		page := "Hello from Go"
+		w.Write([]byte(page))
+	} else {
+		if err != nil {
+			fmt.Fprint(w, err.Error())
+		} else {
+			fmt.Fprint(w, string(requestDump))
+		}
+	}
+	log.Println(string(requestDump))
 }
 
 func main() {
--- docker/httpsgo/httpsgo.go
+++ docker/httpsgo/httpsgo.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 type Configuration struct {
@@ -18,25 +20,31 @@ type Configuration struct {
 var Config Configuration
 
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, true)
 	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Println("Header:", r.Header)
-	fmt.Println("TLS:", r.TLS)
+		fmt.Println("Header:", r.Header)
+		fmt.Println("TLS:", r.TLS)
 
-	// print out all request headers
-	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-	fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q\n", r.Header["Accept"])
+		// print out all request headers
+		fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+		}
+		fmt.Fprintf(w, "Host = %q\n", r.Host)
+		fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
+		fmt.Fprintf(w, "\n\nFinding value of \"Accept\" %q\n", r.Header["Accept"])
 
-	w.WriteHeader(http.StatusOK)
-	page := "Hello Go TLS world!!!"
-	w.Write([]byte(page))
+		w.WriteHeader(http.StatusOK)
+		page := "Hello Go TLS world!!!"
+		w.Write([]byte(page))
+	} else {
+		if err != nil {
+			fmt.Fprint(w, err.Error())
+		} else {
+			fmt.Fprint(w, string(requestDump))
+		}
+	}
+	log.Println(string(requestDump))
 }
 
 func parseConfig(configFile string) error {
