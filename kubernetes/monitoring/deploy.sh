1ba94e88ffb4f51b3d06e11a58b2520d8f47b95a
Valentin <vkuznet@gmail.com>
2020-01-02 21:42:26 +0100

    Add prometheus-test secrets

--- kubernetes/monitoring/deploy.sh
+++ kubernetes/monitoring/deploy.sh
@@ -71,7 +71,12 @@ deploy_secrets()
         echo "delete prometheus-secrets"
         kubectl delete secret prometheus-secrets
     fi
-    ls secrets/prometheus/{*.yml,*.yaml,*.json,console_libraries/*,*.rules} | awk '{ORS=" "; print "--from-file="$1""}' | awk '{print "kubectl create secret generic prometheus-secrets "$0""}' | /bin/sh
+    ls secrets/prometheus/{*.yaml,*.json,console_libraries/*,*.rules} | awk '{ORS=" "; print "--from-file="$1""}' | awk '{print "kubectl create secret generic prometheus-secrets "$0""}' | /bin/sh
+    if [ -n "`kubectl get secrets | grep prometheus-test-secrets`" ]; then
+        echo "delete prometheus-test-secrets"
+        kubectl delete secret prometheus-test-secrets
+    fi
+    ls secrets/prometheus/{*.yaml,*.json,console_libraries/*,*.rules} | awk '{ORS=" "; print "--from-file="$1""}' | awk '{print "kubectl create secret generic prometheus-test-secrets "$0""}' | /bin/sh
 
     # add alermanager secrets
     if [ ! -d secrets/alertmanager ]; then
