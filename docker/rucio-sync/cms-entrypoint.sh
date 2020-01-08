9492eba53901b349be9fa3f8fbd3e5f7706eec55
Eric Vaandering <ericvaandering@gmail.com>
2020-01-08 11:15:06 -0600

    Update cms-entrypoint.sh

--- docker/rucio-sync/cms-entrypoint.sh
+++ docker/rucio-sync/cms-entrypoint.sh
@@ -6,7 +6,8 @@ else
     echo "rucio.cfg not found. will generate one."
     j2 /tmp/rucio.cfg.j2 | sed '/^\s*$/d' > /opt/rucio/etc/rucio.cfg
 fi
-
-~/scripts/new_sync.py --config /etc/sync-config/site-sync.yaml
-
-#sleep infinity
+while true
+do
+    ~/scripts/new_sync.py --config /etc/sync-config/site-sync.yaml
+	sleep 60
+done
