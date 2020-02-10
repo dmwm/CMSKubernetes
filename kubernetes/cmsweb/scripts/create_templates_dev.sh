7fd6a7b39a71ab27137181cc113a3b7e38c29470
mimran <muhammad.imran@cern.ch>
2020-02-10 12:58:26 +0100

    New labels in new version of k8s

--- kubernetes/cmsweb/scripts/create_templates_dev.sh
+++ kubernetes/cmsweb/scripts/create_templates_dev.sh
@@ -15,13 +15,13 @@ echo "Creating: $tmpl"
 openstack \
     --os-project-name "$namespace" coe cluster template create $tmpl \
     --labels admission_control_list="NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota,Priority" \
-    --labels autoscaler_tag="v1.15.2" \
+    --labels autoscaler_tag="v1.17.2" \
     --labels cern_tag="qa" \
     --labels cephfs_csi_enabled="true" \
     --labels cephfs_csi_version="cern-csi-1.0-2" \
     --labels cern_enabled="true" \
     --labels cgroup_driver="cgroupfs" \
-    --labels cloud_provider_tag="v1.15.0" \
+    --labels cloud_provider_tag="v1.17.0" \
     --labels container_infra_prefix="gitlab-registry.cern.ch/cloud/atomic-system-containers/" \
     --labels cvmfs_csi_version="v1.0.0" \
     --labels cvmfs_csi_enabled="true" \
@@ -33,7 +33,7 @@ openstack \
     --labels keystone_auth_enabled="true" \
     --labels kube_csi_enabled="true" \
     --labels kube_csi_version="cern-csi-1.0-2" \
-    --labels kube_tag="v1.15.3" \
+    --labels kube_tag="v1.17.2" \
     --labels logging_type="http" \
     --labels logging_http_destination="http://monit-logs.cern.ch:10012/" \
     --labels logging_producer="cmswebk8s" \
@@ -43,7 +43,7 @@ openstack \
     --labels master_lb_enabled="true" \
     --labels tiller_enabled="true" \
     --coe kubernetes \
-    --image 26666ca8-bda9-4356-982f-4a92845ec361 \
+    --image 5b338766-0fbf-47fa-9d9a-8f9543be9729 \
     --external-network CERN_NETWORK \
     --fixed-network CERN_NETWORK \
     --network-driver flannel \
