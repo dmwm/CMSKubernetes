cd9d5996315e888a15ff34bac32514433617cf2a
mimran <muhammad.imran@cern.ch>
2020-02-17 14:21:12 +0100

    Updating create template dev script

--- kubernetes/cmsweb/scripts/create_templates_dev.sh
+++ kubernetes/cmsweb/scripts/create_templates_dev.sh
@@ -15,34 +15,40 @@ echo "Creating: $tmpl"
 openstack \
     --os-project-name "$namespace" coe cluster template create $tmpl \
     --labels admission_control_list="NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota,Priority" \
-    --labels cern_tag="qa" \
+    --labels autoscaler_tag="v1.17.2" \
+    --labels calico_ipv4pool="10.100.0.0/16" \
+    --labels calico_ipv4pool_ipip="CrossSubnet" \
     --labels cephfs_csi_enabled="true" \
     --labels cephfs_csi_version="cern-csi-1.0-2" \
     --labels cern_enabled="true" \
+    --labels cern_tag="qa" \
     --labels cgroup_driver="cgroupfs" \
     --labels cloud_provider_tag="v1.17.0" \
     --labels container_infra_prefix="gitlab-registry.cern.ch/cloud/atomic-system-containers/" \
-    --labels cvmfs_csi_version="v1.0.0" \
     --labels cvmfs_csi_enabled="true" \
+    --labels cvmfs_csi_version="v1.0.0" \
     --labels cvmfs_tag="qa" \
-    --labels flannel_backend="vxlan" \
-    --labels heat_container_agent_tag="stein-dev-2" \
-    --labels influx_grafana_dashboard_enabled="true" \
+    --labels etcd_tag=3.3.17 \
+    --labels heapster_enabled='false' \
+    --labels heat_container_agent_tag=train-stable-1 \
+    --labels influx_grafana_dashboard_enabled="false" \
     --labels ingress_controller="nginx" \
     --labels keystone_auth_enabled="true" \
     --labels kube_csi_enabled="true" \
     --labels kube_csi_version="cern-csi-1.0-2" \
     --labels kube_tag="v1.17.2" \
-    --labels logging_type="http" \
+    --labels kubeapi_options="--feature-gates=CSINodeInfo=true,CSIDriverRegistry=true --runtime-config=storage.k8s.io/v1alpha1=true" \
+    --labels kubecontroller_options=--feature-gates=CSINodeInfo=true,CSIDriverRegistry=true \
+    --labels kubelet_options=--feature-gates=CSINodeInfo=true,CSIDriverRegistry=true \
     --labels logging_http_destination="http://monit-logs.cern.ch:10012/" \
-    --labels logging_producer="cmswebk8s" \
     --labels logging_include_internal="true" \
+    --labels logging_producer="cmswebk8s" \
+    --labels logging_type="http" \
     --labels manila_enabled="true" \
     --labels manila_version="v0.3.0" \
-    --labels master_lb_enabled="true" \
-    --labels tiller_enabled="true" \
-    --labels calico_ipv4pool="10.100.0.0/16" \
-    --labels calico_ipv4pool_ipip="CrossSubnet" \
+    --labels metrics_server_enabled='true' \
+    --labels tiller_enabled='true' \
+    --labels use_podman='true' \
     --coe kubernetes \
     --image 5b338766-0fbf-47fa-9d9a-8f9543be9729 \
     --external-network CERN_NETWORK \
