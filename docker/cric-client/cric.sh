5e988238cb35ddf1b60d3d1bb0fa607cced08b13
Valentin Kuznetsov <vkuznet@gmail.com>
2020-02-20 09:31:17 -0500

    Move data to cric storage

--- docker/cric-client/cric.sh
+++ docker/cric-client/cric.sh
@@ -2,6 +2,10 @@
 url="https://cms-cric.cern.ch/api/accounts/user/query/?json&preset=roles"
 curl -L -k \
     --key /etc/secrets/proxy --cert /etc/secrets/proxy \
-    -q -o cric.json \
+    -q -o /tmp/cric.json \
     -H "Accept: application/json" \
     "$url"
+# move cric data to cric storage
+if [ -d /cric ]; then
+    mv /tmp/cric.json /cric
+fi
