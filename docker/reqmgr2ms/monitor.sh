bf4a4810b7f1555653e707335eaa2f69cf0b2b1b
Valentin <vkuznet@gmail.com>
2020-06-04 17:07:29 +0200

    Adjust start-up of ms exporters

--- docker/reqmgr2ms/monitor.sh
+++ docker/reqmgr2ms/monitor.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # start process exporter
-pid=`ps axjfwww | grep "wmc-httpd.*reqmgr2" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
-prefix=reqmgr2ms_exporter
-address=":18248"
-nohup process_exporter -pid $pid -prefix $prefix -address $address 2>&1 1>& ${prefix}.log < /dev/null &
+for p in "config-monitor" "config-output" "config-transferor"; do
+    pat="wmc-httpd.*$p"
+    pid=`ps axjfwww | grep "$pat" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+    if [ -n "$pid" ]; then
+        prefix=reqmgr2ms_exporter
+        prefix=`echo $p | sed -e "s,config-,ms_,g"`
+        address=":18248"
+        nohup process_exporter -pid $pid -prefix $prefix -address "$address" 2>&1 1>& ${prefix}.log < /dev/null &
+    fi
+done
 
 # run filebeat
 if [ -f /etc/secrets/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
