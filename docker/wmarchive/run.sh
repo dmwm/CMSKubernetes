812e2926ac622de98536dc8d6ec3be46d67af441
Valentin <vkuznet@gmail.com>
2020-09-07 19:38:42 +0200

    Add run script and default config

---
+++ docker/wmarchive/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+logDir=/data/srv/logs/wma
+if [ ! -d $logDir ]; then
+    logDir=/data
+fi
+config=/data/wmarch_go.json
+if [ -f /etc/secrets/wmarch_go.json ]; then
+    config=/etc/secrets/wmarch_go.json
+fi
+
+# start wmarchive process exporter
+nohup /data/process_monitor.sh ".*wmarch_go.json" wma_exporter ":18200" 15 2>&1 1>& $logDir/wma_exporter.log < /dev/null &
+/data/wmarchive -config=$config
