332ebe7064d6fa30cea9c874877c40dae0552e36
Valentin <vkuznet@gmail.com>
2020-10-19 14:19:13 +0200

    Switch from ssh key to keytab

--- docker/cmsweb/cmsweb-logs.sh
+++ docker/cmsweb/cmsweb-logs.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-usage="cmsweb-logs.sh <ssh-public-key> </path/local-log-area> <user@host:/path/remote-log-area>"
+usage="cmsweb-logs.sh <keytab> </path/local-log-area> <user@host:/path/remote-log-area>"
 if [ $# -ne 3 ]; then
     echo "Usage: $usage"
     exit 1
 fi
 
-sshPubKey=$1
+keytab=$1
 localLogArea=$2
 remoteLogArea=$3
 
+principal=`klist -k "$keytab" | tail -1 | awk '{print $2}'`
+echo "principal=$principal" 2>&1 1>& /dev/null
+kinit $principal -k -t "$keytab" 2>&1 1>& /dev/null
+if [ $? == 1 ]; then
+    echo "Unable to perform kinit" >> $log 2>&1
+    exit 1
+fi
+klist -k "$keytab" 2>&1 1>& /dev/null
+
 if [ -f $sshPubKey ]; then
-    rsync -rm -e "ssh -c aes128-ctr -i $sshPubKey" \
+    rsync -rm \
         --append -f '+s */' -f '+s *.txt' -f '+s *.log*' -f '-s /***/*' \
         $localLogArea $remoteLogArea
 
