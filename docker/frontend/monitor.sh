1ebba4a42ca1cfa580043b35616e798120b68c68
Valentin <vkuznet@gmail.com>
2020-11-05 18:05:41 +0100

    Change file name extension

--- docker/frontend/monitor.sh
+++ docker/frontend/monitor.sh
@@ -6,7 +6,7 @@ suri="http://localhost:$aport/server-status/?auto"
 echo "Start apache_exporter with $suri"
 nohup apache_exporter --scrape_uri $suri --telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
 
-cat > /data/filebeat.conf << EOF
+cat > /data/filebeat.yaml << EOF
 filebeat.inputs:
 - type: log
   enabled: true
