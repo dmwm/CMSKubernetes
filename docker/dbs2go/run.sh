eadb6918bd11facf3b50cf795d5f7b57e40d48b9
Valentin <vkuznet@gmail.com>
2020-12-02 19:10:35 +0100

    Update dbs2go docker build

--- docker/dbs2go/run.sh
+++ docker/dbs2go/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # overwrite proxy if it is present in /etc/proxy
+mkdir -p /data/srv/state/dbs/proxy
+mkdir -p /data/srv/current/auth/proxy
 if [ -f /etc/proxy/proxy ]; then
-    mkdir -p /data/srv/state/dbs/proxy
     ln -s /etc/proxy/proxy /data/srv/state/dbs/proxy/proxy.cert
-    mkdir -p /data/srv/current/auth/proxy
     ln -s /etc/proxy/proxy /data/srv/current/auth/proxy/proxy
 fi
 
+export LD_LIBRARY_PATH=`find /usr/lib/oracle -name libocci.so | sed -e "s,/libocci.so,,g"`
 # start dbs2go server
-cd $GOPATH/src/github.com/vkuznet/dbs2go
 if [ -f /etc/secrets/dbsconfig.json ]; then
     sudo cp /etc/secrets/dbfile /data/srv/current/auth/proxy/dbfile
     sudo chown $USER.$USER /data/srv/current/auth/proxy/dbfile
@@ -18,8 +18,8 @@ if [ -f /etc/secrets/dbsconfig.json ]; then
     ls -la /data/srv/current/auth/proxy/
     echo "X509_USER_PROXY=$X509_USER_PROXY"
     echo "start with /etc/secrets/dbsconfig.json"
-    dbs2go -config /etc/secrets/dbsconfig.json
+    /data/dbs2go -config /etc/secrets/dbsconfig.json
 else
-    echo "start with $PWD/dbsconfig.json"
-    dbs2go -config dbsconfig.json
+    echo "start with $PWD/config.json"
+    /data/dbs2go -config /data/config.json
 fi
