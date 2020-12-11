ed12e09f84c412e081e018069a6da5688eebba80
Valentin <vkuznet@gmail.com>
2020-12-11 18:49:34 +0100

    Adjust rucio sqoop script

--- docker/sqoop/scripts/rucio_replicas.sh
+++ docker/sqoop/scripts/rucio_replicas.sh
@@ -1,14 +1,21 @@
 # set up variables
 BASE_PATH=/project/awg/cms/rucio/
 JDBC_URL=jdbc:oracle:thin:@cms-nrac-scan.cern.ch:10121/CMSR_CMS_NRAC.cern.ch
-USERNAME=TOBEFILLED
-PASSWORD=TOBEFILLED
+if [ -f /etc/secrets/rucio ]; then
+    USERNAME=`cat /etc/secrets/rucio | grep username | awk '{print $2}'`
+    PASSWORD=`cat /etc/secrets/rucio | grep password | awk '{print $2}'`
+else
+    echo "Unable to read Rucio credentials"
+    exit 1
+fi
+LOG_FILE=log/`date +'%F_%H%m%S'`_`basename $0`
 TABLE=cms_rucio_prod.replicas
-
 TZ=UTC
-KRB5CCNAME=/tmp/krb5cc_0
 
-/usr/hdp/sqoop/bin/sqoop import -Dmapreduce.job.user.classpath.first=true -Doraoop.timestamp.string=false -D mapred.chil
-d.java.opts="-Djava.security.egd=file:/dev/../dev/urandom" --connect $JDBC_URL --username $USERNAME --password $PASSWORD
- --num-mappers 100 --fetch-size 10000 --table $TABLE --as-avrodatafile -z --direct --target-dir $BASE_PATH`date +%Y-%m-%
- d`/replicas
+/usr/hdp/sqoop/bin/sqoop import -Dmapreduce.job.user.classpath.first=true \
+    -Doraoop.timestamp.string=false \
+    -D mapred.chil d.java.opts="-Djava.security.egd=file:/dev/../dev/urandom" \
+    --connect $JDBC_URL --username $USERNAME --password $PASSWORD  \
+    --num-mappers 100 --fetch-size 10000 --table $TABLE --as-avrodatafile \
+    -z --direct --target-dir $BASE_PATH`date +%Y-%m-%d`/replicas \
+1>$LOG_FILE.stdout 2>$LOG_FILE.stderr
