f6905ef38a0fdfe36f1de1f346eaa496d95e7846
Eric Vaandering <ericvaandering@gmail.com>
2021-03-08 14:36:59 -0600

    Add proxy to RAL scripts

--- docker/rucio-consistency/RAL_Disk_post.sh
+++ docker/rucio-consistency/RAL_Disk_post.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # This script should run some time after the RAL disk dump script finishes at RAL
-
+export X509_USER_PROXY=/opt/proxy/x509up
 cd /consistency/cms_consistency/RAL
 ./RAL_compare.sh /config/config.yaml /opt/rucio/etc/rucio.cfg T1_UK_RAL_Disk /var/cache/consistency-temp/ /var/cache/consistency-dump/
