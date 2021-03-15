d11a1ffc83c95f5f05b5033544b9a0333c67c64f
Valentin <vkuznet@gmail.com>
2021-03-15 23:38:58 +0100

    Simpligy run.sh script; add procps to the image

--- docker/dbs2go/run.sh
+++ docker/dbs2go/run.sh
@@ -16,17 +16,15 @@ fi
 # check for tnsnames.ora
 if [ -f /etc/secrets/tnsnames.ora ]; then
     export TNS_ADMIN=/etc/secrets/tnsnames.ora
+    if [ ! -f /etc/tnsnames.ora ]; then
+        ln -s /etc/secrets/tnsnames.ora /etc/tnsnames.ora
+    fi
 fi
+echo TNS_ADMIN=$TNS_ADMIN
 
 export LD_LIBRARY_PATH=`find /usr/lib/oracle -name libocci.so | sed -e "s,/libocci.so,,g"`
 # start dbs2go server
 if [ -f /etc/secrets/dbsconfig.json ]; then
-    sudo cp /etc/secrets/dbfile /data/srv/current/auth/proxy/dbfile
-    sudo chown $USER.$USER /data/srv/current/auth/proxy/dbfile
-    sudo chmod 0400 /data/srv/current/auth/proxy/dbfile
-    echo "ls /data/srv/current/auth/proxy"
-    ls -la /data/srv/current/auth/proxy/
-    echo "X509_USER_PROXY=$X509_USER_PROXY"
     echo "start with /etc/secrets/dbsconfig.json"
     /data/dbs2go -config /etc/secrets/dbsconfig.json
 else
