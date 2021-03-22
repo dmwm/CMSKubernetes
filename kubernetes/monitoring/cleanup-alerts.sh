5cd16fa0f959eb93a4629c58f743c8866a5866c7
Valentin <vkuznet@gmail.com>
2021-03-22 14:11:31 +0100

    New script to clean-up alerts

---
+++ kubernetes/monitoring/cleanup-alerts.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+amtool=/afs/cern.ch/user/v/valya/alert
+services="SSB GGUS"
+date="2021-03-20T20:52:00Z"
+hosts="http://cms-monitoring-ha1.cern.ch:30093 http://cms-monitoring-ha2.cern.ch:30093 http://cms-monitoring.cern.ch:30093"
+for srv in $services; do
+    for amurl in $hosts; do
+    echo "clean-up $amurl service=$srv"
+        $amtool -service=$srv | grep Undefined | \
+            awk '{print ""AMTOOL" -ends=\""DATE"\" -name="$1" -amurl="AMURL""}' \
+            DATE=$date AMURL=$amurl AMTOOL=$amtool | /bin/sh
+    done
+done
