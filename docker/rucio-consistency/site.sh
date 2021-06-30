b7d836a46859466135e5378fb959ffd296e35a30
Eric Vaandering <ericvaandering@gmail.com>
2021-06-30 12:48:25 -0500

    Fix proxy permission issues

--- docker/rucio-consistency/site.sh
+++ docker/rucio-consistency/site.sh
@@ -2,7 +2,11 @@
 
 cd /consistency/cms_consistency/site_cmp3
 
-export PYTHON=python3 
+cp /opt/proxy/x509up /tmp/x509up
+chmod 600 /tmp/x509up
+export X509_USER_PROXY=/tmp/x509up
+
+export PYTHON=python3
 
 ./site_cmp3.sh \
   /config/config.yaml \
@@ -10,7 +14,7 @@ export PYTHON=python3
   $1 \
   /var/cache/consistency-temp \
   /var/cache/consistency-dump \
-  /opt/proxy/x509up                   
+  /tmp/x509up
 
 
 
