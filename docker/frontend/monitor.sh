c2daf3751026d929da74d374cb9500bc9c02aac1
muhammadimranfarooqi <muhammad.imran@ncp.edu.pk>
2021-07-06 16:08:04 +0200

    Removing apache exporter from frontend image

--- docker/frontend/monitor.sh
+++ docker/frontend/monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # determine which port httpd server uses
-aport=`cat /data/srv/state/frontend/server.conf | grep Listen | grep 80 | awk '{print $2}'`
-suri="http://localhost:$aport/server-status/?auto"
-echo "Start apache_exporter with $suri"
-nohup apache_exporter --scrape_uri $suri --telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
+#aport=`cat /data/srv/state/frontend/server.conf | grep Listen | grep 80 | awk '{print $2}'`
+#suri="http://localhost:$aport/server-status/?auto"
+#echo "Start apache_exporter with $suri"
+#nohup apache_exporter --scrape_uri $suri --telemetry.address ":18443" 2>&1 1>& apache_exporter.log < /dev/null &
 
 NAME=`hostname -s`
 #if [ -n $MY_POD_NAME ]; then
