e408c9f950fd8d41d99121eb059fb9399ec9b1c6
muhammadimranfarooqi <muhammad.imran@ncp.edu.pk>
2021-07-20 11:48:49 +0200

    Removing exporter from wmarchive

--- docker/wmarchive/run.sh
+++ docker/wmarchive/run.sh
@@ -9,5 +9,5 @@ if [ -f /etc/secrets/wmarch_go.json ]; then
 fi
 
 # start wmarchive process exporter
-nohup /data/process_monitor.sh ".*wmarch_go.json" wma_exporter ":18200" 15 2>&1 1>& $logDir/wma_exporter.log < /dev/null &
-/data/wmarchive -config=$config
+#nohup /data/process_monitor.sh ".*wmarch_go.json" wma_exporter ":18200" 15 2>&1 1>& $logDir/wma_exporter.log < /dev/null &
+nohup /data/wmarchive -config=$config
