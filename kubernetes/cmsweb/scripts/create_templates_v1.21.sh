806ee605c81dc35ba7518ff3bb31c2f97d943655
mimran <muhammad.imran@cern.ch>
2021-09-21 11:14:28 +0200

    Adding new service autodqmwebapp

--- kubernetes/cmsweb/scripts/create_templates_v1.21.sh
+++ kubernetes/cmsweb/scripts/create_templates_v1.21.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 namespace=${OS_PROJECT_NAME:-"CMS Web"}
-tmpl=cmsweb-template-v-1.21.1-1-`date +%Y%m%d`
+tmpl=rucio-template-v-1.21.1-1-`date +%Y%m%d`
 usage="create_templates.sh <tmpl_name, if not provided will use $tmpl>"
 if [ "$1" != "" ]; then
     if [ "$1" == "-help" ] || [ "$1" == "-h" ]; then
