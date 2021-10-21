17078c6ae54b4baa8cb1813d55670ade8f5cb9aa
Valentin <vkuznet@gmail.com>
2021-10-21 20:36:10 +0200

    start monitor only if DBS2GO_PROCESS_MONITOR env is set

--- docker/dbs2go/monitor.sh
+++ docker/dbs2go/monitor.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# start process exporters
-process_monitor.sh ".*dbs2go" dbs2go_exporter ":18258" 15 2>&1 1>& dbs2go_exporter.log &
+if [ -n $DBS2GO_PROCESS_MONITOR ]; then
+    # start process exporters
+    process_monitor.sh ".*dbs2go" dbs2go_exporter ":18259" 15 2>&1 1>& dbs2go_exporter.log &
 
-# run filebeat
-if [ -f /etc/secrets/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
-    ldir=/tmp/filebeat
-    mkdir -p $ldir/data
-    nohup /usr/bin/filebeat \
-        -c /etc/secrets/filebeat.yaml \
-        --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
+    # run filebeat
+    if [ -f /etc/secrets/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
+        ldir=/tmp/filebeat
+        mkdir -p $ldir/data
+        nohup /usr/bin/filebeat \
+            -c /etc/secrets/filebeat.yaml \
+            --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
+    fi
 fi
