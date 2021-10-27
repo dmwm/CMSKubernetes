b9b9c90af700f449a8714460d5d3d05c8c117d3e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2021-10-27 14:37:42 +0200

    adds cert-manager as sidecar

--- docker/cert-manager/run.sh
+++ docker/cert-manager/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cert=$1
+cert_path=$1
 if [ -n  "${AMURL}" ] ; then
     amurl=$AMURL
 else
@@ -8,22 +8,25 @@ fi
 
 while :
 do
-    certTime=`openssl x509 -enddate -noout -in $cert | sed -e "s,notAfter=,,g"`
-    tstamp=`date --date="$certTime" +"%s"`
-    now=`date +"%s"`
-    if [ $tstamp -lt $now ] ; then
-        msg="Certificate $cert expired on $certTime"
-        echo $msg
-        expire=`date -d '+5 min' --rfc-3339=ns | tr ' ' 'T'`
-        if [ -f /data/amtool ] ; then
-            /data/amtool alert add cert_alert \
-                alertname=cert_alert severity=high tag=test alert=amtool \
-                --end=$expire \
-                --annotation=date="`date`" \
-                --annotation=hostname="`hostname`" \
-                --annotation=message="$msg" \
-                --alertmanager.url=$amurl
+    for cert in "$cert_path"/*; do
+        certTime=`openssl x509 -enddate -noout -in $cert | sed -e "s,notAfter=,,g"`
+        tstamp=`date --date="$certTime" +"%s"`
+        now=`date +"%s"`
+        echo "Checking $cert with expiration $certTime"
+        if [ $tstamp -lt $now ] ; then
+            msg="Certificate $cert expired on $certTime"
+            echo $msg
+            expire=`date -d '+5 min' --rfc-3339=ns | tr ' ' 'T'`
+            if [ -f /data/amtool ] ; then
+                /data/amtool alert add cert_alert \
+                    alertname=cert_alert severity=high tag=test alert=amtool \
+                    --end=$expire \
+                    --annotation=date="`date`" \
+                    --annotation=hostname="`hostname`" \
+                    --annotation=message="$msg" \
+                    --alertmanager.url=$amurl
+            fi
         fi
-    fi
+    done
     sleep 10
 done
