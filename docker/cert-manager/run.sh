f034fc5a1275d365aca25305f2afd7f83608567e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2021-10-28 11:13:52 +0200

    deploys cert-manager a sidecar pod

--- docker/cert-manager/run.sh
+++ docker/cert-manager/run.sh
@@ -11,15 +11,13 @@ do
     for cert in "$cert_path"/*; do
         certTime=`openssl x509 -enddate -noout -in $cert | sed -e "s,notAfter=,,g"`
         tstamp=`date --date="$certTime" +"%s"`
-        now=`date +"%s"`
-        echo "Checking $cert with expiration $certTime"
-        if [ $tstamp -lt $now ] ; then
-            msg="Certificate $cert expired on $certTime"
-            echo $msg
-            expire=`date -d '+5 min' --rfc-3339=ns | tr ' ' 'T'`
+        alertDate=`date -d "+15 days" +"%s"`
+        if [  ! -z  "$certTime" ] && [ $tstamp -lt $alertDate ] ; then
+            msg="Certificate $cert on pod $POD_NAME running on node $NODE_NAME will expire on $certTime"
+            expire=`date -d '+15 days' --rfc-3339=ns | tr ' ' 'T'`
             if [ -f /data/amtool ] ; then
                 /data/amtool alert add cert_alert \
-                    alertname=cert_alert severity=high tag=test alert=amtool \
+                    alertname=cert_alert severity=high tag=cmsweb alert=amtool \
                     --end=$expire \
                     --annotation=date="`date`" \
                     --annotation=hostname="`hostname`" \
