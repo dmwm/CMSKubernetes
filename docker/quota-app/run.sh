9c8da99b053ec5710d70c37c7b59db8b00fe6378
muhammadimranfarooqi <muhammad.imran@ncp.edu.pk>
2021-11-25 10:40:34 +0100

    Adjusting run script quota ap

--- docker/quota-app/run.sh
+++ docker/quota-app/run.sh
@@ -2,11 +2,14 @@
 ### This script relies on provided configuration files which will be
 ### be mounted to /etc/secrets area
 
-# start cron daemon
 echo "Docker container has been started"
+
+/data/CMSKubernetes/kubernetes/cmsweb/scripts/quota.sh /etc/secrets/env.sh
 
 # Setup a cron schedule
 echo "0 * * * * /data/CMSKubernetes/kubernetes/cmsweb/scripts/quota.sh /etc/secrets/env.sh >> /var/log/cron.log 2>&1" > scheduler.txt
+
+# start cron daemon
 
 crontab scheduler.txt
 sudo /usr/sbin/crond -n
