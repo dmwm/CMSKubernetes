0cf8d155ce6d5281d4b5dc018780423dfb977728
Eric Vaandering <ericvaandering@gmail.com>
2022-02-15 09:15:21 -0600

    Fix permission issues with proxy

--- docker/rucio-consistency/unmerged_site.sh
+++ docker/rucio-consistency/unmerged_site.sh
@@ -2,7 +2,10 @@
 
 cd /consistency/cms_consistency/wm
 
-export PYTHON=python3 
-export X509_USER_PROXY=/opt/proxy/x509up
+cp /opt/proxy/x509up /tmp/x509up
+chmod 600 /tmp/x509up
+export X509_USER_PROXY=/tmp/x509up
+
+export PYTHON=python3
 
 ./wm_scan.sh /unmerged-config/config.yaml $1  /var/cache/consistency-dump/unmerged
