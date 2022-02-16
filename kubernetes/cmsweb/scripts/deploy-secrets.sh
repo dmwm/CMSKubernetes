2af675b3925b2008bd3f262176d044cd9d0da636
mimran <muhammad.imran@cern.ch>
2022-02-16 12:05:40 +0100

    Updating deploy secrets for client secrets substitution

--- kubernetes/cmsweb/scripts/deploy-secrets.sh
+++ kubernetes/cmsweb/scripts/deploy-secrets.sh
@@ -42,13 +42,27 @@ conf=$3
         	cp $cmsweb_crt $conf/frontend-ds/hostcert.pem
     	fi
     fi
-
-
 	secretdir=$conf/$srv
         # the underscrore is not allowed in secret names
         osrv=$srv
         srv=`echo $srv | sed -e "s,_,,g"`
         files=""
+
+    if [ "$srv" == "auth-proxy-server" ] || [ "$srv" == "x509-proxy-server" ] || [ "$srv" == "scitokens-proxy-server" ] ; then
+        if [ -d $secretdir ] && [ -n "`ls $secretdir`" ] && [ -f $secretdir/client.secrets ]; then
+                export CLIENT_SECRET=`grep CLIENT_SECRET $secretdir/client.secrets | head -n1 | awk '{print $2}'`
+                export CLIENT_ID=`grep CLIENT_ID $secretdir/client.secrets | head -n1 | awk '{print $2}'`
+                export IAM_CLIENT_ID=`grep IAM_CLIENT_ID $secretdir/client.secrets | head -n1 | awk '{print $2}'`
+                export IAM_CLIENT_SECRET=`grep IAM_CLIENT_SECRET $secretdir/client.secrets | head -n1 | awk '{print $2}'`
+	        if [ -f $secretdir/config.json ]; then
+	                echo "$(jq '.client_id=env.CLIENT_ID' $secretdir/config.json)" > $secretdir/config.json
+        	        echo "$(jq '.client_secret=env.CLIENT_SECRET' $secretdir/config.json)" > $secretdir/config.json
+	                echo "$(jq '.iam_client_id=env.IAM_CLIENT_ID' $secretdir/config.json)" > $secretdir/config.json
+        	        echo "$(jq '.iam_client_secret=env.IAM_CLIENT_SECRET' $secretdir/config.json)" > $secretdir/config.json
+		fi
+        fi
+    fi 
+
         if [ -d $secretdir ] && [ -n "`ls $secretdir`" ]; then
         	for fname in $secretdir/*; do
                 	files="$files --from-file=$fname"
