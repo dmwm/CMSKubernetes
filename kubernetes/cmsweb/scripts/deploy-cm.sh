959551b9da8689ca900d7f2208ca8054184239d5
mimran <muhammad.imran@cern.ch>
2022-05-10 11:31:28 +0200

    Adjustment of deploy-secret/cm scripts for keys

--- kubernetes/cmsweb/scripts/deploy-cm.sh
+++ kubernetes/cmsweb/scripts/deploy-cm.sh
@@ -11,20 +11,16 @@ cluster_name=`kubectl config get-clusters | grep -v NAME`
 ns=$1
 srv=$2
 conf=$3
-secretref=${4:-https://openstack.cern.ch:9311/v1/secrets/35145b52-18af-47a3-90d0-1861c51a9c65}
 
-if [[ "$cluster_name" == cmsweb-test[1-9]* ]] ; then
-   secretref="https://openstack.cern.ch:9311/v1/secrets/010bee01-f50f-40e6-b954-e24eae51d8d3"
+tmpDir=/tmp/$USER/sops
+if [ -d $tmpDir ]; then
+   rm -rf $tmpDir
 fi
+mkdir -p $tmpDir
+cd $tmpDir
 
 if [ -z "`command -v sops`" ]; then
   # download soap in tmp area
-  tmpDir=/tmp/$USER/sops
-  if [ -d $tmpDir ]; then
-    rm -rf $tmpDir
-  fi
-  mkdir -p $tmpDir
-  cd $tmpDir
   wget -O sops https://github.com/mozilla/sops/releases/download/v3.7.2/sops-v3.7.2.linux.amd64
   chmod u+x sops
   mkdir -p $HOME/bin
@@ -42,12 +38,11 @@ fi
   	exit 1
     fi
 
-    mkdir -p $HOME/.openstack/config/sops/age
-    if [ ! -f $HOME/.openstack/config/sops/age/openstack-keys.txt ]; then
-      openstack secret get $secretref --payload_content_type application/octet-stream --file $HOME/.openstack/config/sops/age/openstack-keys.txt
-    fi
+
     sopskey=$SOPS_AGE_KEY_FILE
-    export SOPS_AGE_KEY_FILE="$HOME/.openstack/config/sops/age/openstack-keys.txt"
+    kubectl get secrets $ns-keys-secrets -n $ns --template="{{index .data \"$ns-keys.txt\" | base64decode}}" > "$ns-keys.txt"
+    export SOPS_AGE_KEY_FILE="$tmpDir/$ns-keys.txt"
+    echo "Key file: $SOPS_AGE_KEY_FILE"
 
 	cmdir=$conf/$srv
         osrv=$srv
--- kubernetes/cmsweb/scripts/deploy-secrets.sh
+++ kubernetes/cmsweb/scripts/deploy-secrets.sh
@@ -2,7 +2,7 @@
 # helper script to deploy given service with given tag to k8s infrastructure
 
 if [ $# -ne 3 ]; then
-    echo "Usage: deploy-secrets.sh <namespace> <service-name> <path_to_configuration> <reference_of_decryption_barbican_secret>"
+    echo "Usage: deploy-secrets.sh <namespace> <service-name> <path_to_configuration>"
     exit 1
 fi
 
@@ -11,20 +11,22 @@ cluster_name=`kubectl config get-clusters | grep -v NAME`
 ns=$1
 srv=$2
 conf=$3
-secretref=${4:-https://openstack.cern.ch:9311/v1/secrets/35145b52-18af-47a3-90d0-1861c51a9c65}
+#secretref=${4:-https://openstack.cern.ch:9311/v1/secrets/35145b52-18af-47a3-90d0-1861c51a9c65}
 
-if [[ "$cluster_name" == cmsweb-test[1-9]* ]] ; then
-   secretref="https://openstack.cern.ch:9311/v1/secrets/010bee01-f50f-40e6-b954-e24eae51d8d3"
+#if [[ "$cluster_name" == cmsweb-test[1-9]* ]] ; then
+#   secretref="https://openstack.cern.ch:9311/v1/secrets/010bee01-f50f-40e6-b954-e24eae51d8d3"
+#fi
+
+tmpDir=/tmp/$USER/sops
+if [ -d $tmpDir ]; then
+   rm -rf $tmpDir
 fi
+mkdir -p $tmpDir
+cd $tmpDir
+
 
 if [ -z "`command -v sops`" ]; then
   # download soap in tmp area
-  tmpDir=/tmp/$USER/sops
-  if [ -d $tmpDir ]; then
-    rm -rf $tmpDir
-  fi
-  mkdir -p $tmpDir
-  cd $tmpDir
   wget -O sops https://github.com/mozilla/sops/releases/download/v3.7.2/sops-v3.7.2.linux.amd64
   chmod u+x sops
   mkdir -p $HOME/bin
@@ -43,12 +45,17 @@ fi
     fi
    
     # Get SOPS decryption key (if it's not there) and set it as the default decryption file
-    mkdir -p $HOME/.openstack/config/sops/age
-    if [ ! -f $HOME/.openstack/config/sops/age/openstack-keys.txt ]; then
-      openstack secret get $secretref --payload_content_type application/octet-stream --file $HOME/.openstack/config/sops/age/openstack-keys.txt
-    fi
-    sopskey=$SOPS_AGE_KEY_FILE
-    export SOPS_AGE_KEY_FILE="$HOME/.openstack/config/sops/age/openstack-keys.txt"
+
+    
+#    mkdir -p $HOME/.openstack/config/sops/age
+#    if [ ! -f $HOME/.openstack/config/sops/age/openstack-keys.txt ]; then
+#      openstack secret get $secretref --payload_content_type application/octet-stream --file $HOME/.openstack/config/sops/age/openstack-keys.txt
+#    fi
+    export sopskey=$SOPS_AGE_KEY_FILE
+    kubectl get secrets $ns-keys-secrets -n $ns --template="{{index .data \"$ns-keys.txt\" | base64decode}}" > "$ns-keys.txt"
+    export SOPS_AGE_KEY_FILE="$tmpDir/$ns-keys.txt"
+    echo "Key file: $SOPS_AGE_KEY_FILE"
+
     # check (and copy if necessary) hostkey/hostcert.pem files in configuration area of frontend
 
     if [ "$srv" == "frontend" ] ; then
