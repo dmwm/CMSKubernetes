ee6cd4e68c905e017b5636c8540ea87bfb207e50
micsucmed <m.succar@uniandes.edu.co>
2022-07-06 16:53:13 +0200

    newdqmgui kerberos configuration added

---
+++ docker/newdqmgui/monitor.sh
@@ -0,0 +1,8 @@
+export keytab=/etc/secrets/msuccarm.keytab
+principal=`klist -k "$keytab" | tail -1 | awk '{print $2}'`
+kinit $principal -k -t "$keytab" 2>&1 1>& /dev/null
+if [ $? == 1 ]; then
+    echo "Unable to perform kinit."
+    echo "If you are installing a DQM GUI on lxplus or any other private machine, please comment lines from this block and proceed as usual"
+    exit 1
+fi
