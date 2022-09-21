da23870c7d23b4fa5c839cd0c060d887afa6b9f5
Valentin <vkuznet@gmail.com>
2022-09-21 15:29:59 +0200

    New image to test python Cherrypy HTTP server

---
+++ docker/httppy/server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import time
+import cherrypy
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        out = "CherryPy response headesr"
+        headers = cherrypy.response.headers
+        for hdr in cherrypy.response.headers:
+            out += "\n" + hdr
+        out += "CherryPy request headesr"
+        for hdr in cherrypy.request.headers:
+            out += "\n" + hdr
+        return "Hello from CherryPy\n"+out
+
+def application(environ, start_response):
+    cherrypy.tree.mount(HelloWorld(), '/', None)
+    return cherrypy.tree(environ, start_response)
+
+if __name__ == '__main__':
+#     cherrypy.config.update({'server.thread_pool': 2, 'server.socket_timeout': 0})
+    cherrypy.quickstart(HelloWorld())
+
