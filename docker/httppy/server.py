db1e0b4649d8ff41ab838b375e3d81adecb2d604
Valentin <vkuznet@gmail.com>
2022-09-21 15:34:08 +0200

    Add healthz method

--- docker/httppy/server.py
+++ docker/httppy/server.py
@@ -14,6 +14,9 @@ class HelloWorld(object):
         for hdr in cherrypy.request.headers:
             out += "\n" + hdr
         return "Hello from CherryPy\n"+out
+    @cherrypy.expose
+    def healthz(self):
+        return "ok"
 
 def application(environ, start_response):
     cherrypy.tree.mount(HelloWorld(), '/', None)
@@ -22,4 +25,3 @@ def application(environ, start_response):
 if __name__ == '__main__':
 #     cherrypy.config.update({'server.thread_pool': 2, 'server.socket_timeout': 0})
     cherrypy.quickstart(HelloWorld())
-
