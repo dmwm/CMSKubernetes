56b98c3481c447fe03fd0f9ea111246d703ca1d1
Valentin <vkuznet@gmail.com>
2022-11-03 23:13:16 +0100

    adjust log name

--- docker/pypi/reqmgr2ms-rulecleaner/run.sh
+++ docker/pypi/reqmgr2ms-rulecleaner/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# script to start ReqMgr2
+# script to start reqmgr2ms-rulecleaner
 
 STATEDIR=/data/state
 LOGDIR=/data/logs
 CFGFILE=/etc/secrets/config.py
 mkdir -p /data/{logs,state}
-wmc-httpd -r -d $STATEDIR -l "|rotatelogs $LOGDIR/reqmgr2-%Y%m%d-`hostname -s`.log 86400" $CFGFILE
+wmc-httpd -r -d $STATEDIR -l "|rotatelogs $LOGDIR/reqmgr2ms-rulecleaner-%Y%m%d-`hostname -s`.log 86400" $CFGFILE
