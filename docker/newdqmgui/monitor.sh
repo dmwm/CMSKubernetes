ad6d23a11714b3b8dcde5156c7c9737352a298f4
micsucmed <m.succar@uniandes.edu.co>
2022-11-18 15:51:17 +0100

    Persistant kerberos authentication on container with K5start

--- docker/newdqmgui/monitor.sh
+++ docker/newdqmgui/monitor.sh
@@ -4,7 +4,10 @@ if [ -f /etc/secrets/msuccarm.keytab ]; then
     echo "FOUND KEYTABFILE"
     export keytab=/etc/secrets/msuccarm.keytab
     principal=`klist -k "$keytab" | tail -1 | awk '{print $2}'`
-    kinit $principal -k -t "$keytab" 2>&1 1>& /dev/null
+#    kinit $principal -k -t -r "5d" "$keytab"
+    echo "ABOUT TO PERFORM K5START"
+    k5start $principal -K 30 -b -f "$keytab"
+    echo "JUST PERFOMED K5START"
     if [ $? == 1 ]; then
         echo "Unable to perform kinit."
         echo "If you are installing a DQM GUI on lxplus or any other private machine, please comment lines from this block and proceed as usual"
