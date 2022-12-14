76c85b321392bb8ce7e86555a03581a8f80cfcaf
Alan Malta Rodrigues <alan.malta@cern.ch>
2022-12-14 10:59:53 -0500

    remove proxy env var; use diff dir for hmac; use nohup to run process

--- docker/pypi/dmwm-base/run.sh
+++ docker/pypi/dmwm-base/run.sh
@@ -13,14 +13,14 @@ mkdir -p $LOGDIR
 mkdir -p $STATEDIR
 mkdir -p $AUTHDIR
 mkdir -p $CONFIGDIR
-mkdir -p $AUTHDIR/proxy
 mkdir -p $AUTHDIR/../wmcore-auth
 
 # environment variables required to run some of the WMCore services
 export REQMGR_CACHE_DIR=$STATEDIR
 export WMCORE_CACHE_DIR=$STATEDIR
 
-# overwrite host PEM files in /data/srv area
+# overwrite host PEM files in /data/srv area by the robot certificate
+# Note that the proxy file is not required and used
 if [ -f /etc/robots/robotkey.pem ]; then
     sudo cp /etc/robots/robotkey.pem $AUTHDIR/dmwm-service-key.pem
     sudo cp /etc/robots/robotcert.pem $AUTHDIR/dmwm-service-cert.pem
@@ -34,29 +34,10 @@ if [ -e $AUTHDIR/dmwm-service-cert.pem ] && [ -e $AUTHDIR/dmwm-service-key.pem ]
     export X509_USER_KEY=$AUTHDIR/dmwm-service-key.pem
 fi
 
-# overwrite proxy if it is present in /etc/proxy
-if [ -f /etc/proxy/proxy ]; then
-    sudo cp /etc/proxy/proxy $AUTHDIR/proxy/proxy
-    export X509_USER_PROXY=$AUTHDIR/proxy/proxy
-fi
-
 # overwrite header-auth key file with one from secrets
-
 if [ -f /etc/hmac/hmac ]; then
-    if [ -f $AUTHDIR/../header-auth-key ]; then
-        sudo rm $AUTHDIR/../header-auth-key
-    fi
     sudo cp /etc/hmac/hmac $AUTHDIR/../wmcore-auth/header-auth-key
-    sudo chown $USER.$USER $AUTHDIR/../wmcore-auth/header-auth-key
-
-    if [ -f $AUTHDIR/header-auth-key ]; then
-        sudo rm $AUTHDIR/header-auth-key
-    fi
-    sudo cp /etc/hmac/hmac $AUTHDIR/header-auth-key
-    sudo chown $USER.$USER $AUTHDIR/header-auth-key
-    # why do we need this hmac file at /auth directory as well?
-    sudo mkdir -p /auth/wmcore-auth
-    sudo ln -s /data/srv/current/auth/$srv/header-auth-key /auth/wmcore-auth/header-auth-key
+    sudo chown 0600 $USER.$USER $AUTHDIR/../wmcore-auth/header-auth-key
 fi
 
 # use service configuration files from /etc/secrets if they are present
@@ -84,4 +65,4 @@ done
 export PYTHONPATH=$PYTHONPATH:/etc/secrets:$AUTHDIR/$fname
 
 # start the service
-wmc-httpd -r -d $STATEDIR -l "|rotatelogs $LOGDIR/$srv-%Y%m%d-`hostname -s`.log 86400" $CFGFILE
+nohup wmc-httpd -r -d $STATEDIR -l "|rotatelogs $LOGDIR/$srv-%Y%m%d-`hostname -s`.log 86400" $CFGFILE &
