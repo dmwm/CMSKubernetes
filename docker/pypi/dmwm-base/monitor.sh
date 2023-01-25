8f1322b6a39b789d71447d24fa703c0fa73ce934
Valentin Kuznetsov <vkuznet@gmail.com>
2023-01-25 15:06:12 -0500

    Monotoring script for all dmwm images

---
+++ docker/pypi/dmwm-base/monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# start process exporter
+configs="config config-monitor config-output config-transferor config-ruleCleaner config-unmerged"
+for p in $configs; do
+    pat="wmc-httpd.*$p"
+    pid=`ps axjfwww | grep "$pat" | grep -v grep | grep -v process_monitor | grep -v " 1 " | awk '{print $1}'`
+    if [ -n "$pid" ]; then
+        app=`grep ^main.application /etc/secrets/${p}.py | sed -e 's,#.*,,g' | awk '{split($0,a,"="); print a[2]}' | sed -e "s, ,,g" -e 's,",,g'`
+        prefix=process_exporter_${app}
+        port=`grep main.port /etc/secrets/config.py | sed -e 's,#.*,,g' | awk '{split($0,a,"="); print a[2]}' | sed -e "s, ,,g"`
+        address=":1${port}"
+        echo "Start ${prefix} on ${address}"
+        nohup process_exporter -pid $pid -prefix $prefix -address "$address" 2>&1 1>& ${prefix}.log < /dev/null &
+        #cpyAddr=`echo ${address} | sed "s,8,9,g"`
+        #echo "Start cpy_exporter on ${cpyAddr}"
+        #nohup cpy_exporter -address "$address" 2>&1 1>& cpy_${prefix}.log < /dev/null &
+    fi
+done
