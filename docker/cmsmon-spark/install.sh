8b929b6ac365bcbc989a4c8f0f2fe4a0c3caa9c9
Nikodemas Tuckus <ntuckus@gmail.com>
2023-07-10 11:15:07 +0200

    Add opensearchpy lib to dockerfile

--- docker/cmsmon-spark/install.sh
+++ docker/cmsmon-spark/install.sh
@@ -26,6 +26,6 @@ fi
 zip -r CMSMonitoring.zip CMSMonitoring/src/python/CMSMonitoring/*
 
 # -- Install python modules
-pip install --no-cache-dir stomp.py==7.0.0 click pyspark pandas numpy schema seaborn matplotlib plotly requests==2.29
+pip install --no-cache-dir click matplotlib numpy opensearch-py~=2.1 pandas plotly pyspark requests==2.29 schema seaborn stomp.py==7.0.0
 
 echo "Info: CMSSPARK_TAG=${CMSSPARK_TAG} , CMSMON_TAG=${CMSMON_TAG}, HADOOP_CONF_DIR=${HADOOP_CONF_DIR}, PATH=${PATH}, PYTHONPATH=${PYTHONPATH}, PYSPARK_PYTHON=${PYSPARK_PYTHON}, PYSPARK_DRIVER_PYTHON=${PYSPARK_DRIVER_PYTHON}"
