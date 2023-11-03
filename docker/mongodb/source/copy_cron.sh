61f95280f7f3cc9db7d27fc0f04b52c03ea0a270
Aroosha Pervaiz <aroosha.pervaiz@cern.ch>
2023-11-03 12:44:51 +0100

    Automating the kinit operation.

--- docker/mongodb/source/copy_cron.sh
+++ docker/mongodb/source/copy_cron.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# Print a message to indicate the script is running
+echo "copy_cron.sh is running"
 
 if [ "$ENVIRONMENT" = "prod" ]; then
+  # Copy the production cron file
+  echo "Copying mongo-prod.cron"
   cp /data/tools/mongo-prod.cron /data/tools/mongo.cron
 elif [ "$ENVIRONMENT" = "preprod" ]; then
+  # Copy the development cron file
+  echo "Copying mongo-preprod.cron"
   cp /data/tools/mongo-preprod.cron /data/tools/mongo.cron
 elif [ "$ENVIRONMENT" = "test" ]; then
+  # Copy the test cron file
+  echo "Copying mongo-test.cron"
   cp /data/tools/mongo-test.cron /data/tools/mongo.cron
 else
   echo "Unsupported environment: $ENVIRONMENT"
@@ -13,4 +22,7 @@ fi
 
 # Set up the cron job
 crontab /data/tools/mongo.cron
+
+# Add another cronjob to get kerberos token once everyday
+(crontab -l ; echo "0 0 * * * /root/run.sh") | crontab -
 
--- docker/mongodb/source/run.sh
+++ docker/mongodb/source/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 ### This script relies on provided keytab file which will be
 ### be mounted to /etc/krb area
-if [ -f /etc/krb ]; then
+if [ -d /etc/krb ]; then
+  echo "Starting the kinit operation!"
   export keytab=/etc/krb/cmsweb.keytab
   principal=`klist -k "$keytab" | tail -1 | awk '{print $2}'`
-  kinit $principal -k -t "$keytab" 2>&1 1>& /dev/null
+  kinit $principal -k -t "$keytab" >/dev/null 2>&1
   if [ $? == 1 ]; then
     echo "Unable to perform kinit operation for cmsweb keytab."
     exit 1
   fi
 fi
+
