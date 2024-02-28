8fd215554c2b708dc17129184f321744132db54e
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2024-02-28 14:13:08 +0100

    Resolve the user to run the database server at runtime
    Set Default tag to 10.6.5

--- docker/pypi/wmagent-mariadb/run.sh
+++ docker/pypi/wmagent-mariadb/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-manage init-mariadb 2>&1 | tee -a run.log
-manage start-mariadb 2>&1 | tee -a run.log
+manage init-mariadb  2>&1 | tee -a $MDB_LOG_DIR/run.log
+manage start-mariadb 2>&1 | tee -a $MDB_LOG_DIR/run.log
 
 echo "Start sleeping....zzz"
 while true; do sleep 10; done
