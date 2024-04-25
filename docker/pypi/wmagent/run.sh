c7fec388edcc7c26b0a739411065a1465e6172d6
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2024-04-25 12:15:00 +0200

    Fix broken command prompts for user agnostic containers

--- docker/pypi/wmagent/run.sh
+++ docker/pypi/wmagent/run.sh
@@ -2,6 +2,10 @@
 
 ### Basic initialization wrapper for WMAgent to serve as the main entry point for the WMAgent Docker container
 
+# Set command prompt for the running user inside the container
+cat <<EOF >> ~/.bashrc
+export PS1="(WMAgent-\$WMA_TAG) [\u@\h:\W]\$ "
+EOF
 
 echo "Start initialization"
 ./init.sh | tee -a $WMA_LOG_DIR/init.log || true
