e743f3cb94bca055f496b346c1e3a10dd93560dc
A. Pervaiz <77356768+arooshap@users.noreply.github.com>
2024-05-03 15:51:28 +0200

    Update copy_cron.sh

--- docker/frontend/copy_cron.sh
+++ docker/frontend/copy_cron.sh
@@ -5,7 +5,7 @@ if [ "$ENVIRONMENT" = "k8s-prod" ]; then
 elif [ "$ENVIRONMENT" = "k8s-preprod" ]; then
   # Copy the development cron file
   echo "Copying authmap-preprod.cron"
-  echo "*/15 * * * * /tmp/authmap-prod.sh" > /tmp/authmap.cron
+  echo "*/15 * * * * /tmp/authmap-preprod.sh" > /tmp/authmap.cron
 elif [[ "$ENVIRONMENT" == k8s-test* ]]; then
   # Copy the test cron file
   echo "Copying authmap-test.cron"
