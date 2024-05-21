f33b3d1549a6460609db2e55b94b9e7e5c112257
Valentin <vkuznet@gmail.com>
2024-05-21 15:22:47 +0200

    new script to extract secrets

---
+++ kubernetes/cmsweb/scripts/extract_secrets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -ne 3 ]; then
+    echo "Extracts all secret files for given namespace and secret into provided output directory"
+    echo "Usage: extract_secrets.sh <namespace> <secret> <output directory>"
+    exit 1
+fi
+
+ns=$1
+secret=$2
+odir=$3
+files=`kubectl describe secrets -n $1 $2 | grep bytes | awk '{print $1}' | sed -e "s/://g"`
+for sfile in $files; do
+    echo "extract $sfile"
+    kubectl get secrets -n $ns $secret -o yaml | grep $sfile | head -1 | awk '{print $2}' | base64 -d > $odir/$sfile
+done
