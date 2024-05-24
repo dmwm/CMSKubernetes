ca9926d20680fad639f2135d57fbe3376750e7b7
Kenyi Hurtado <khurtado@nd.edu>
2024-05-24 15:09:59 -0400

    WMAgent: When invoking `docker stop`, kill leftover children (#1488)
    Trap SIGTERM signal, stop WMAgent services and then kill any leftover children
    from the run.sh parent process recursively using rkill (from pslist utility)
    
    Dependency: pslist (from container)

--- docker/pypi/wmagent/run.sh
+++ docker/pypi/wmagent/run.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+_service_gracefull_exit() {
+    ppid=$$
+    echo "Full list of currently running processes:"
+    ps auxf
+
+    echo -e "\nStopping WMAgent"
+    manage stop-agent
+
+    echo -e "\nList of all child processes of pid $ppid upon agent graceful exit:"
+    pslist $ppid
+
+    echo -e "\nKilling children processes recursively"
+    rkill -9 $ppid
+
+    echo -e "\nFull list of currently running processes after killing children:"
+    ps auxf
+}
+
+# Trap SIGTERM signal (e.g.: when doing docker stop)
+trap _service_gracefull_exit SIGTERM
+
 ### Basic initialization wrapper for WMAgent to serve as the main entry point for the WMAgent Docker container
 wmaUser=$(id -un)
 wmaGroup=$(id -gn)
@@ -20,4 +41,5 @@ echo "Start initialization"
 $WMA_ROOT_DIR/init.sh | tee -a $WMA_LOG_DIR/init.log || true
 
 echo "Start sleeping now ...zzz..."
-sleep infinity
+sleep infinity &
+wait
