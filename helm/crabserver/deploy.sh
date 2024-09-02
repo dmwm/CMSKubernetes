a5f420091721a01b2b8d5e2dfdc1d347307cdd79
Thanayut Seethongchuen <thanayut.seethongchuen@cern.ch>
2024-09-02 16:48:28 +0200

    fix deploy.sh script

--- helm/crabserver/deploy.sh
+++ helm/crabserver/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -euo pipefail
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+ENVNAME=$1
 
 # hash table of clusters nickname-name:
 declare -A cluster_map=([prod]=prod)
@@ -11,20 +11,31 @@ cluster_map[testbed]=testbed
 cluster_map[test2]=test2
 cluster_map[test11]=test11
 cluster_map[test12]=test12
+cluster_map[test14]=test14
+
+declare -A valuefile_map=([prod]=prod)
+valuefile_map[prod]=values-prod.yaml
+valuefile_map[preprod]=values-preprod.yaml
+valuefile_map[testbed]=values-testbed.yaml
+valuefile_map[test2]=values-testx.yaml
+valuefile_map[test11]=values-testx.yaml
+valuefile_map[test12]=values-testx.yaml
+valuefile_map[test14]=values-testx.yaml
+
 if [[ $# -ne 1 ]]; then
     echo "Usage: deploy.sh ENVNAME"
-    echo " ENVNAME=(prod|preprod|testbed|test2|test11|test12)"
+    echo " ENVNAME=(prod|preprod|testbed|test2|test11|test12|test14)"
     exit 1
 fi
-desired_cluster="${cluster_map[$1]}"
+desired_cluster="${cluster_map[$ENVNAME]}"
+valuefile="${valuefile_map[$ENVNAME]}"
 
 # make sure that your current context points to the desired cluster
 current_cluster=$(kubectl config view -o json | jq '.["current-context"] as $context | .["contexts"][] | select(.name | contains($context))| .context.cluster')
-
-if [[ $current_cluster =~ $desired_cluster ]]; then 
-  echo "deploying to $desired_cluster"; 
-  helm template crabserver . -f values.yaml -f values-${1}-pypi.yaml | kubectl -n crab apply -f -
-else 
-  echo "wrong cluster: your are connected to $current_cluster"; 
+set -x
+if [[ $current_cluster =~ $desired_cluster ]]; then
+  echo "deploying to $desired_cluster";
+  helm template crabserver . -f values.yaml -f "${valuefile}" | kubectl -n crab apply -f -
+else
+  echo "Error: wrong cluster. your are connected to $current_cluster";
 fi
-
