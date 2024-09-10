62be47b4fbe7349fe77f01e9fc36922ae0dc0627
Aroosha Pervaiz <aroosha.pervaiz@cern.ch>
2024-09-10 18:54:51 +0200

    Adding functionality with PROXY

--- kubernetes/cmsweb/scripts/test_k8s_aps.sh
+++ kubernetes/cmsweb/scripts/test_k8s_aps.sh
@@ -2,6 +2,7 @@
 ##H Usage: test_k8s_aps.sh <base_url> <url_list_file>
 ##H
 
+# Replace Perl usage with standard UNIX tools
 if [ "$1" == "-h" ] || [ "$1" == "-help" ] || [ "$1" == "--help" ] || [ "$1" == "help" ] || [ "$1" == "" ]; then
     grep "##H" $0 | sed -e "s,##H,,g"
     exit 1
@@ -29,24 +30,44 @@ fi
 # Function to perform the request with automatic redirect following
 perform_request() {
     local target_url="$1"
-    final_status_code=$(curl -L -i --key $X509_USER_KEY --cert $X509_USER_CERT -o /dev/null -w '%{http_code}' -s "$target_url")
+    local cert="$2"
+    local key="$3"
+    final_status_code=$(curl -L -i --key "$key" --cert "$cert" -o /dev/null -w '%{http_code}' -s "$target_url")
     echo "  $target_url -> HTTP code: $final_status_code"
 }
 
 # Read the list of URLs from the file and process each
-current_section=""
-while read -r line; do
-    # If the line is a comment, treat it as a section heading
-    if [[ "$line" =~ ^#.*$ ]]; then
-        section_header="${line/#\# /}"  # Remove "# " from the start of the comment
-        if [[ "$section_header" != "$current_section" ]]; then
-            current_section="$section_header"
-            echo
-            echo "== $current_section =="
+run_tests() {
+    local cert="$1"
+    local key="$2"
+    current_section=""
+    while read -r line; do
+        # If the line is a comment, treat it as a section heading
+        if [[ "$line" =~ ^#.*$ ]]; then
+            section_header="${line/#\# /}"  # Remove "# " from the start of the comment
+            if [[ "$section_header" != "$current_section" ]]; then
+                current_section="$section_header"
+                echo
+                echo "== $current_section =="
+            fi
+        # If the line is not empty or a comment, process the URL
+        elif [[ ! -z "$line" ]]; then
+            perform_request "${base_url}${line}" "$cert" "$key"
         fi
-    # If the line is not empty or a comment, process the URL
-    elif [[ ! -z "$line" ]]; then
-        perform_request "${base_url}${line}"
-    fi
-done < "$url_list_file"
+    done < "$url_list_file"
+}
+
+# Run tests with X509_USER_CERT and X509_USER_KEY
+echo "### Running tests with X509_USER_CERT and X509_USER_KEY"
+run_tests "$X509_USER_CERT" "$X509_USER_KEY"
+
+# Generate and use the proxy certificate
+echo "### Running tests with X509_USER_PROXY"
+unset X509_USER_PROXY
+voms-proxy-init -voms cms -rfc
+export X509_USER_PROXY=/tmp/x509up_u`id -u`
+
+echo
+echo "### Testing with proxy: $X509_USER_PROXY"
+run_tests "$X509_USER_PROXY" "$X509_USER_PROXY"
 
