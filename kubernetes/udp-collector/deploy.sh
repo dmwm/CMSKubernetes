709e800fe3b4290d256e95b20d54580c1449b7ee
Nikodemas Tuckus <ntuckus@gmail.com>
2024-10-25 15:40:49 +0200

    Prepare udp-collector for Kubernetes

--- kubernetes/udp-collector/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname "$0")
-node=`kubectl get node | egrep -v "NAME|master" | awk '{print $1}'`
-kubectl label node $node role=ingress --overwrite
-kubectl create secret generic udp-secrets --from-file=secrets/udp/udp_server.json
-kubectl apply -f udp-server.yaml --validate=false
-kubectl apply -f ingress.yaml
-$BASEDIR/adjust_nginx.sh
