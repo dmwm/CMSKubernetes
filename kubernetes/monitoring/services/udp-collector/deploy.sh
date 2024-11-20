8852baa6a653a7adab176858863ab24bd455203a
Nikodemas Tuckus <ntuckus@gmail.com>
2024-11-20 10:55:08 +0100

    Add comments and additional checks

--- kubernetes/monitoring/services/udp-collector/deploy.sh
+++ kubernetes/monitoring/services/udp-collector/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
-kubectl create secret generic udp-secrets --from-file=secrets/udp/udp_server.json
+SECRET_FILE="$BASEDIR/secrets/udp/udp_server.json"
+
+if [ ! -f "$SECRET_FILE" ]; then
+  echo "Error: $SECRET_FILE not found."
+  exit 1
+fi
+
+kubectl create secret generic udp-secrets --from-file="$SECRET_FILE"
 kubectl apply -f udp-server.yaml --validate=false
