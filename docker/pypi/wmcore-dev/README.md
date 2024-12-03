0816642a432485f4555eece2dbc48ad6df7069ed
Dennis Lee <dylee@fnal.gov>
2024-12-03 17:08:19 -0600

    Added stable makefile command
    Use version number for stable
    Copy TestScripts and ContainerScripts to /home/cmsbld in entrypoint

--- docker/pypi/wmcore-dev/README.md
+++ docker/pypi/wmcore-dev/README.md
@@ -5,7 +5,11 @@ This image is used for running unit tests and linting for the [WMCore](https://g
 
 ## Usage
 1. To build the image, run `make build`
-2. Push to the CERN registry with `make push`
+2. Push to the CERN registry with `make push` or `make push-stable`
+  * Special account privileges are needed to push to the CERN registry.
+  * `*-stable` tagged releases are not automatically cleared from the registry.
+  * Currently, the date is used as a tag for `make push`, while `make push-stable` uses a version number.
 
 ## Maintainers
 [@d-ylee](https://github.com/d-ylee)
+[@khurtado](https://github.com/khurtado)
--- docker/pypi/wmcore-dev/entrypoint.sh
+++ docker/pypi/wmcore-dev/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # add WMCore repository, if in Jenkins
 if [[ -z "${BUILD_ID}" ]]; then
-    echo "No BUILD_ID set"
+    echo "No BUILD_ID set, we in dev mode"
 else
     echo "BUILD_ID set, setting up for CI/CD"
     # give proper permissions to home directory
@@ -11,12 +11,19 @@ else
     USERN=$(id -un ${MY_ID})
 
     su - $USERN << EOSU
-    git clone https://github.com/dmwm/WMCore
 
     pushd /home/cmsbld
 
+    # clone main repo
+    git clone https://github.com/dmwm/WMCore
+
     # clone jenkins-test scripts
-    git clone https://github.com/dmwm/WMCore-Jenkins .
+    git clone https://github.com/dmwm/WMCore-Jenkins
+
+    # TODO: Move scripts to a more sensible location in WMCore-Jenkins
+    cp -r WMCore-Jenkins/docker/wmcore-dev/TestScripts .
+    cp -r WMCore-Jenkins/docker/wmcore-dev/ContainerScripts .
+    cp -r WMCore-Jenkins/docker/wmcore-dev/etc .
     popd
 EOSU
 fi
