86fedd5c23acb5c597ca15818b783f79be30dca6
Antonio Linares <a.linares@cern.ch>
2025-06-04 10:43:00 +0200

    New docker image for t0wmadatasvc

--- docker/t0wmadatasvc/install.sh
+++ docker/t0wmadatasvc/install.sh
@@ -1,63 +1,7 @@
 #!/bin/bash
+#No longer using rpm for packaging t0wmadatasvc
 
-ARCH=slc7_amd64_gcc630
-VER=HG2309b
-REPO="comp"
-AREA=/data/cfg/admin
-PKGS="admin backend t0wmadatasvc@2.0.7"
-SERVER=cmsrep.cern.ch
+TAG=2.1.1
+pip install --upgrade pip
+pip install --no-cache-dir git+https://github.com/dmwm/t0wmadatasvc.git@$TAG
 
-cd $WDIR
-git clone https://github.com/dmwm/deployment.git cfg
-mkdir $WDIR/srv
-
-cd $WDIR/cfg
-git reset --hard $VER
-
-# adjust deploy script to use k8s host name
-cmsk8s_prod=${CMSK8S:-https://cmsweb.cern.ch}
-cmsk8s_prep=${CMSK8S:-https://cmsweb-testbed.cern.ch}
-cmsk8s_dev=${CMSK8S:-https://cmsweb-dev.cern.ch}
-cmsk8s_priv=${CMSK8S:-https://cmsweb-test.web.cern.ch}
-sed -i -e "s,https://cmsweb.cern.ch,$cmsk8s_prod,g" \
-    -e "s,https://cmsweb-testbed.cern.ch,$cmsk8s_prep,g" \
-    -e "s,https://cmsweb-dev.cern.ch,$cmsk8s_dev,g" \
-    -e "s,https://\`hostname -f\`,$cmsk8s_priv,g" \
-    t0wmadatasvc/deploy
-
-# Deploy services
-# we do not use InstallDev script directly since we want to capture the status of
-# install step script. Therefore we call Deploy script and capture its status every step
-cd $WDIR
-curl -sO http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh
-sh -x ./bootstrap.sh -architecture $ARCH -path $WDIR/tmp/$VER/sw -repository $REPO -server $SERVER setup
-# deploy services
-$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s prep $WDIR/srv "$PKGS"
-if [ $? -ne 0 ]; then
-    cat $WDIR/srv/.deploy/*-prep.log
-    exit 1
-fi
-$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s sw $WDIR/srv "$PKGS"
-if [ $? -ne 0 ]; then
-    cat $WDIR/srv/.deploy/*-sw.log
-    exit 1
-fi
-$WDIR/cfg/Deploy -A $ARCH -R comp@$VER -r comp=$REPO -t $VER -w $SERVER -s post $WDIR/srv "$PKGS"
-if [ $? -ne 0 ]; then
-    cat $WDIR/srv/.deploy/*-post.log
-    exit 1
-fi
-
-# comment out usage of port 8443 in k8s setup
-#files=`find /data/srv/$VER/sw/$ARCH -type f | xargs grep ":8443" | awk '{print $1}' | sed -e "s,:,,g" | grep py$`
-#for fname in $files; do
-#    sed -i -e "s,:8443,,g" $fname
-#done
-
-# Adjust ServerMonitor to be specific
-sed -i -e "s#ServerMonitor/2.0#ServerMonitor-t0wmadatasvc#g" /data/srv/current/config/admin/ServerMonitor
-
-# adjust crontabs
-crontab -l | egrep -v "reboot|ProxyRenew|LogArchive|ServerMonitor" > /tmp/mycron
-crontab /tmp/mycron
-rm /tmp/mycron
