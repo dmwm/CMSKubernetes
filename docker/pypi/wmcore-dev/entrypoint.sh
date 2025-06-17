a56979f89b884d74a23cfeabb6285f9a61795fa9
Dennis Lee <dylee@fnal.gov>
2025-06-17 09:18:35 -0500

    Removed script directories, update entrypoint with permissions

--- docker/pypi/wmcore-dev/entrypoint.sh
+++ docker/pypi/wmcore-dev/entrypoint.sh
@@ -5,23 +5,23 @@ if [[ -z "${BUILD_ID}" ]]; then
     echo "No BUILD_ID set, we in dev mode"
 else
     echo "BUILD_ID set, setting up for CI/CD"
-    # give proper permissions to home directory
-    #chown -R ${MY_ID}:${MY_GROUP} /home/cmsbld
 
-    #USERN=$(id -un ${MY_ID})
+    # give proper ownership and permissions to home directory
+    chown -R ${MY_ID}:${MY_GROUP} /home/cmsbld
+    chmod -R 755 /home/cmsbld
 
     pushd /home/cmsbld
 
     # clone main repo
-    git clone https://github.com/dmwm/WMCore
+    git clone https://github.com/${WMCORE_ORG:-dmwm}/WMCore
 
     # clone jenkins-test scripts
-    git clone https://github.com/dmwm/WMCore-Jenkins
+    git clone --depth 1 --branch ${WMCORE_JENKINS_TAG:-master} https://github.com/${WMCORE_JENKINS_ORG:-dmwm}/WMCore-Jenkins
 
-    # TODO: Move scripts to a more sensible location in WMCore-Jenkins
-    cp -r WMCore-Jenkins/docker/wmcore-dev/TestScripts .
-    cp -r WMCore-Jenkins/docker/wmcore-dev/ContainerScripts .
-    cp -r WMCore-Jenkins/docker/wmcore-dev/etc .
+    cp -r WMCore-Jenkins/TestScripts .
+    cp -r WMCore-Jenkins/ContainerScripts .
+    cp -r WMCore-Jenkins/etc .
+
     popd
 
 fi
