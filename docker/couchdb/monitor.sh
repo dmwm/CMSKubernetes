3b37d20c5c45498a3ca19f2d48a74f68cc7d5103
Valentin Kuznetsov <vkuznet@gmail.com>
2025-06-24 10:17:37 -0400

    Changes to include couchdb-prometheus-exporter

--- docker/couchdb/monitor.sh
+++ docker/couchdb/monitor.sh
@@ -4,10 +4,9 @@
 COUCH_CONFIG=/data/srv/current/auth/couchdb/couchdb_config.ini
 # test if file is not zero size
 if [ -s "${COUCH_CONFIG}" ]; then
-    sudo cp /etc/secrets/$fname /data/srv/current/auth/$srv/$fname
-    sudo chown $USER.$USER /data/srv/current/auth/$srv/$fname
-    nohup couchdb-prometheus-exporter -telemetry.address=":9984" -logtostderr=true \
-      --config=$COUCH_CONFIG -databases.views=false 2>&1 1>& couchdb_exporter.log < /dev/null &
+    nohup /data/couchdb-prometheus-exporter -telemetry.address=":9984" \
+      --config=$COUCH_CONFIG -databases.views=false --databases=_all_dbs 2>&1 1>& \
+      /data/couchdb_exporter.log < /dev/null &
 else
     echo "ERROR: couchdb_config.ini file is empty and prometheus exporter cannot be started!"
 fi
@@ -19,4 +18,6 @@ if [ -f /etc/secrets/filebeat.yaml ] && [ -f /usr/bin/filebeat ]; then
     nohup /usr/bin/filebeat \
         -c /etc/secrets/filebeat.yaml \
         --path.data $ldir/data --path.logs $ldir -e 2>&1 1>& $ldir/log < /dev/null &
+else
+  echo "ERROR: filebeat process is not started as /etc/secrets/filebeat.yaml does not exist"
 fi
